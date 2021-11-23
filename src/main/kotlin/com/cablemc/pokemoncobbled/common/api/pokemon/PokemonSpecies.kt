c15e7acfb9db0a6c26023689a0aafc82b65d3397
Landon <46169669+landonjw@users.noreply.github.com>
2021-11-23 17:40:39 -0500

    Expose species in PokemonSpecies

--- src/main/kotlin/com/cablemc/pokemoncobbled/common/api/pokemon/PokemonSpecies.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/common/api/pokemon/PokemonSpecies.kt
@@ -4,14 +4,23 @@ import com.cablemc.pokemoncobbled.common.pokemon.Species
 import com.cablemc.pokemoncobbled.common.pokemon.SpeciesLoader
 
 object PokemonSpecies {
-    private val allSpecies = mutableListOf<Species>()
-
     val BULBASAUR = register(SpeciesLoader.loadFromAssets("bulbasaur"))
     val EEVEE = register(SpeciesLoader.loadFromAssets("eevee"))
+
+    private val allSpecies = mutableListOf(
+        BULBASAUR,
+        EEVEE
+    )
+    val species: List<Species>
+        get() = species.toList()
 
     fun register(species: Species): Species {
         allSpecies.add(species)
         return species
+    }
+
+    fun getByName(name: String): Species? {
+        return allSpecies.firstOrNull { species -> species.name == name }
     }
 
     fun count() = allSpecies.size
