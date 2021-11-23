237965f41e8031aa0d4cc71063f2c121ef1ba77d
Landon <46169669+landonjw@users.noreply.github.com>
2021-11-22 21:40:32 -0500

    Add initial rendering for blockbench models

--- src/main/kotlin/com/cablemc/pokemoncobbled/client/PokemonCobbledClient.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/client/PokemonCobbledClient.kt
@@ -1,14 +1,15 @@
 package com.cablemc.pokemoncobbled.client
 
 import com.cablemc.pokemoncobbled.client.keybinding.PartySendBinding
-import com.cablemc.pokemoncobbled.client.render.models.smd.renderer.SmdModelRenderer
-import com.cablemc.pokemoncobbled.client.render.pokemon.PokemonRenderer
+import com.cablemc.pokemoncobbled.client.render.blockbench.EeveeModel
+import com.cablemc.pokemoncobbled.client.render.pokemon.EeveeRenderer
 import com.cablemc.pokemoncobbled.mod.PokemonCobbledMod
 import net.minecraft.client.renderer.entity.EntityRenderer
 import net.minecraft.client.renderer.entity.EntityRendererProvider
 import net.minecraft.client.renderer.entity.EntityRenderers
 import net.minecraft.world.entity.Entity
 import net.minecraft.world.entity.EntityType
+import net.minecraftforge.client.ForgeHooksClient
 import net.minecraftforge.fmlclient.registry.ClientRegistry
 import net.minecraftforge.fmllegacy.RegistryObject
 
@@ -17,15 +18,20 @@ object PokemonCobbledClient {
         ClientRegistry.registerKeyBinding(PartySendBinding)
     }
 
-    val pokemonRendererProvider = EntityRendererProvider { PokemonRenderer(it, SmdModelRenderer()) }
+    val pokemonRendererProvider = EntityRendererProvider { EeveeRenderer(it) }
 
     fun registerRenderers() {
         registerEntityRenderer(PokemonCobbledMod.entityRegistry.POKEMON) { pokemonRendererProvider.create(it) }
     }
 
+    fun registerLayerDefinitions() {
+        ForgeHooksClient.registerLayerDefinition(EeveeModel.LAYER_LOCATION, EeveeModel::createBodyLayer)
+    }
+
     fun initialize() {
         registerKeybinds()
         registerRenderers()
+        registerLayerDefinitions()
     }
 
     private fun <T : Entity> registerEntityRenderer(
