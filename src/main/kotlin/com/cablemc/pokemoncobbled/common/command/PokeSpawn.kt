1956044508d7a96303433f9ac3629872485b34c2
Qumoo <76853697+Qumoo@users.noreply.github.com>
2021-11-28 11:26:47 +0100

    changed argument to PokemonArgumentType

--- src/main/kotlin/com/cablemc/pokemoncobbled/common/command/PokeSpawn.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/common/command/PokeSpawn.kt
@@ -1,9 +1,11 @@
 package com.cablemc.pokemoncobbled.common.command
 
 import com.cablemc.pokemoncobbled.common.api.pokemon.PokemonSpecies
+import com.cablemc.pokemoncobbled.common.command.argument.PokemonArgumentType
 import com.cablemc.pokemoncobbled.common.command.provider.PokemonNameSuggestions
 import com.cablemc.pokemoncobbled.common.entity.pokemon.PokemonEntity
 import com.cablemc.pokemoncobbled.common.pokemon.Pokemon
+import com.mojang.brigadier.Command
 import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.arguments.StringArgumentType
 import com.mojang.brigadier.context.CommandContext
@@ -16,24 +18,23 @@ object PokeSpawn {
     fun register(dispatcher : CommandDispatcher<CommandSourceStack>) {
         val command = Commands.literal("pokespawn")
             .requires { it.hasPermission(4) }
-            .then(Commands.argument("species", StringArgumentType.word())
-                .suggests(PokemonNameSuggestions())
-                .executes { execute(it) })
+            .then(
+                Commands.argument("pokemon", PokemonArgumentType.pokemon())
+                    .executes { execute(it) })
         dispatcher.register(command)
     }
 
     private fun execute(context: CommandContext<CommandSourceStack>) : Int {
-        val speciesName = StringArgumentType.getString(context, "species")
-        val speciesArg = PokemonSpecies.getByName(speciesName) ?: PokemonSpecies.species.random()
+        val pkm = PokemonArgumentType.getPokemon(context, "pokemon")
         val player = context.source.playerOrException
         val pokemonEntity = PokemonEntity(player.level as ServerLevel)
         pokemonEntity.let {
-            it.pokemon = Pokemon().apply { species = speciesArg }
+            it.pokemon = Pokemon().apply { species = pkm }
             it.dexNumber.set(it.pokemon.species.nationalPokedexNumber)
         }
         player.level.addFreshEntity(pokemonEntity)
         pokemonEntity.setPos(player.position())
-        return 1
+        return Command.SINGLE_SUCCESS
     }
 
 }
