433d2b0409285f4de3a20add83d1e8af71368112
Paul <paul_l_preston@hotmail.com>
2021-11-30 12:04:08 +1000

    Improve documentation in SettableObservable

--- src/main/kotlin/com/cablemc/pokemoncobbled/common/api/reactive/SettableObservable.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/common/api/reactive/SettableObservable.kt
@@ -2,7 +2,7 @@ package com.cablemc.pokemoncobbled.common.api.reactive
 
 /**
  * A type of [Observable] that wraps around a value. This means you can immediately get the current value of
- * the observable, and can set a new value yourself.
+ * the observable, and it's expected that external users can set the value.
  *
  * @author Hiroku
  * @since November 26th, 2021
@@ -26,6 +26,11 @@ open class SettableObservable<T>(private var value: T) : Observable<T> {
         subscriptions.remove(subscription)
     }
 
+    /**
+     * Emits the new value only if it is different from the current value.
+     *
+     * If you want to emit the new value no matter what, use [SettableObservable.emit] directly.
+     */
     open fun set(newValue: T) {
         if (this.value?.equals(newValue) == true || (this.value == null && newValue == null)) {
             return
@@ -33,10 +38,15 @@ open class SettableObservable<T>(private var value: T) : Observable<T> {
         emit(newValue)
     }
 
+    /** Emits and stores the new value. */
     open fun emit(newValue: T) {
         this.value = newValue
         subscriptions.forEach { it.handle(newValue) }
     }
 
+    /**
+     * Gets the current value of this observable. This is unique to [SettableObservable]s as [Observable]
+     * does not necessarily have state.
+     */
     open fun get() = this.value
 }
