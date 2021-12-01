c574adf70b3e82c530114cf77ed54cb50ea7466a
Landon <46169669+landonjw@users.noreply.github.com>
2021-11-30 23:00:53 -0500

    Add rudimentary pokeball entity animation

--- src/main/kotlin/com/cablemc/pokemoncobbled/client/render/pokeball/PokeBallRenderer.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/client/render/pokeball/PokeBallRenderer.kt
@@ -3,7 +3,6 @@ package com.cablemc.pokemoncobbled.client.render.pokeball
 import com.cablemc.pokemoncobbled.client.render.models.blockbench.repository.PokeBallModelRepository
 import com.cablemc.pokemoncobbled.common.entity.pokeball.PokeBallEntity
 import com.mojang.blaze3d.vertex.PoseStack
-import com.mojang.math.Quaternion
 import com.mojang.math.Vector3f
 import net.minecraft.client.renderer.MultiBufferSource
 import net.minecraft.client.renderer.entity.EntityRenderer
@@ -11,7 +10,6 @@ import net.minecraft.client.renderer.entity.EntityRendererProvider
 import net.minecraft.client.renderer.entity.ItemRenderer
 import net.minecraft.client.renderer.texture.OverlayTexture
 import net.minecraft.resources.ResourceLocation
-import net.minecraft.util.Mth
 
 class PokeBallRenderer<T : PokeBallEntity>(context: EntityRendererProvider.Context) : EntityRenderer<T>(context) {
 
@@ -33,8 +31,9 @@ class PokeBallRenderer<T : PokeBallEntity>(context: EntityRendererProvider.Conte
     ) {
         val model = PokeBallModelRepository.getModel(pEntity.pokeBall).entityModel
         pMatrixStack.pushPose()
-        pMatrixStack.mulPose(Vector3f.YP.rotationDegrees(Mth.lerp(pPartialTicks, pEntity.yRotO, pEntity.yRot) - 90.0f))
-        pMatrixStack.mulPose(Vector3f.ZP.rotationDegrees(Mth.lerp(pPartialTicks, pEntity.xRotO, pEntity.xRot) + 90.0f))
+        pMatrixStack.translate(0.0, 1.8, 0.0)
+        pMatrixStack.mulPose(Vector3f.YP.rotationDegrees(pEntity.tickCount * 15f))
+        pMatrixStack.mulPose(Vector3f.ZP.rotationDegrees(180f))
         val vertexconsumer = ItemRenderer.getFoilBufferDirect(pBuffer, model.renderType(getTextureLocation(pEntity)), false, false)
         model.renderToBuffer(pMatrixStack, vertexconsumer, pPackedLight, OverlayTexture.NO_OVERLAY, 1.0f, 1.0f, 1.0f, 1.0f)
         pMatrixStack.popPose()
--- src/main/kotlin/com/cablemc/pokemoncobbled/common/entity/pokeball/PokeBallEntity.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/common/entity/pokeball/PokeBallEntity.kt
@@ -2,11 +2,12 @@ package com.cablemc.pokemoncobbled.common.entity.pokeball
 
 import com.cablemc.pokemoncobbled.common.item.ItemRegistry
 import com.cablemc.pokemoncobbled.common.pokemon.pokeball.PokeBall
+import net.minecraft.network.protocol.Packet
 import net.minecraft.world.entity.EntityType
 import net.minecraft.world.entity.projectile.ThrowableItemProjectile
 import net.minecraft.world.item.Item
 import net.minecraft.world.level.Level
-import net.minecraft.world.level.block.Rotation
+import net.minecraftforge.fmllegacy.network.NetworkHooks
 
 abstract class PokeBallEntity(
     val pokeBall: PokeBall,
@@ -15,5 +16,9 @@ abstract class PokeBallEntity(
 ) : ThrowableItemProjectile(entityType, level) {
 
     override fun getDefaultItem(): Item = ItemRegistry.POKE_BALL.get()
+
+    override fun getAddEntityPacket(): Packet<*> {
+        return NetworkHooks.getEntitySpawningPacket(this)
+    }
 
 }
