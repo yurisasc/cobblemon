e5ba26b12515e6740f8706c4e78a86a4fac13cec
Qumoo <me@qumo.at>
2021-12-04 15:22:27 +0100

    remove unused import

--- src/main/kotlin/com/cablemc/pokemoncobbled/common/item/PokeBallItem.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/common/item/PokeBallItem.kt
@@ -6,7 +6,6 @@ import com.cablemc.pokemoncobbled.common.pokemon.pokeball.PokeBall
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResultHolder
 import net.minecraft.world.entity.player.Player
-import net.minecraft.world.item.CreativeModeTab
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.level.Level
 
