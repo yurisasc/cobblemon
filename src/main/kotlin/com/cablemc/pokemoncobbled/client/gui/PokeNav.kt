826c41a80a264cc3dc2a7693f01d35b0b72ccb57
Qumoo <me@qumo.at>
2021-12-05 13:52:32 +0100

    removed unused import

--- src/main/kotlin/com/cablemc/pokemoncobbled/client/gui/PokeNav.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/client/gui/PokeNav.kt
@@ -10,7 +10,6 @@ import net.minecraft.client.gui.components.Button
 import net.minecraft.client.gui.components.ImageButton
 import net.minecraft.client.gui.screens.Screen
 import net.minecraft.client.renderer.GameRenderer
-import net.minecraft.network.chat.TextComponent
 import net.minecraft.network.chat.TranslatableComponent
 import net.minecraft.resources.ResourceLocation
 
