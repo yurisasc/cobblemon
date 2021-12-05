c4b30d71655a2d8a37806d5f91cf8604a848f72f
Qumoo <me@qumo.at>
2021-12-05 13:21:05 +0100

    added comments and renamed PositionAwareButton to fit class name

--- src/main/kotlin/com/cablemc/pokemoncobbled/client/gui/PokeNav.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/client/gui/PokeNav.kt
@@ -43,7 +43,7 @@ class PokeNav: Screen(TextComponent("PokeNav")) {
     private var aboutToClose = false
 
     override fun init() {
-        // Selection
+        // Selection Button
         selection = PositionAwareImageButton(
             0, 0,
             (width - backgroundWidth) / 2 + SPACING - 2, (height - backgroundHeight) / 2 + SPACING - 2,
@@ -52,17 +52,17 @@ class PokeNav: Screen(TextComponent("PokeNav")) {
             selectionTexture, buttonWidth + 4, buttonHeight + 4
         ) {}
 
-        // Pokedex
+        // Pokedex Button
         buttons.add(posImageButtonOf(0, 0, pokedex, this::onPressPokedex))
 
-        // Bag
+        // Bag Button
         buttons.add(posImageButtonOf(1, 0, bag, this::onPressBag))
 
-        // ?
+        // ? Button
         buttons.add(posImageButtonOf(0, 1, question, this::onPressQuestion))
 
         buttons.forEach { button ->
-            rows[button.posY]++
+            rows[button.posY]++ // To know how many buttons are in one row
             addRenderableOnly(button)
         }
         addRenderableWidget(selection)
@@ -72,24 +72,24 @@ class PokeNav: Screen(TextComponent("PokeNav")) {
 
     override fun keyPressed(pKeyCode: Int, pScanCode: Int, pModifiers: Int): Boolean {
         if(pKeyCode == InputConstants.KEY_RIGHT || pKeyCode == InputConstants.KEY_D) {
-            if(currentSelectionPos.first == MAX_BUTTONS_PER_ROW || currentSelectionPos.first == rows[currentSelectionPos.second] - 1) {
-                changeSelectionToPos(0, currentSelectionPos.second)
+            if(currentSelectionPos.first == rows[currentSelectionPos.second] - 1) { // If the current position is the last button in the row
+                changeSelectionToPos(0, currentSelectionPos.second) // Reset to first button of row
             } else {
-                changeSelectionToPos(currentSelectionPos.first + 1, currentSelectionPos.second)
+                changeSelectionToPos(currentSelectionPos.first + 1, currentSelectionPos.second) // Go one Button to the right
             }
         }
         if(pKeyCode == InputConstants.KEY_LEFT || pKeyCode == InputConstants.KEY_A) {
-            if(currentSelectionPos.first == 0) {
-                changeSelectionToPos(rows[currentSelectionPos.second] - 1, currentSelectionPos.second)
+            if(currentSelectionPos.first == 0) { // If the current button is the first one in the row
+                changeSelectionToPos(rows[currentSelectionPos.second] - 1, currentSelectionPos.second) // Go to the last Button in the row
             } else {
-                changeSelectionToPos(currentSelectionPos.first - 1, currentSelectionPos.second)
+                changeSelectionToPos(currentSelectionPos.first - 1, currentSelectionPos.second) // Go one Button to the right
             }
         }
         if(pKeyCode == InputConstants.KEY_UP || pKeyCode == InputConstants.KEY_W) {
-            if(currentSelectionPos.second == 0) {
+            if(currentSelectionPos.second == 0) { // If the current Button is in the uppermost row
                 val x = currentSelectionPos.first
-                val y = rows.indexOfLast { it != 0 }
-                if(x > rows[y] - 1) {
+                val y = rows.indexOfLast { it != 0 } // Can have entries with 0 (0 representing that there is now button in that column) so the last filled one
+                if(x > rows[y] - 1) { // If the row above (can also be the last due to going up in the first row puts you in the last row) has fewer buttons than the current one also go back one button
                     changeSelectionToPos(x - 1, y)
                 } else {
                     changeSelectionToPos(x, y)
@@ -99,12 +99,12 @@ class PokeNav: Screen(TextComponent("PokeNav")) {
             }
         }
         if(pKeyCode == InputConstants.KEY_DOWN || pKeyCode == InputConstants.KEY_S) {
-            if(currentSelectionPos.second == rows.indexOfLast { it != 0 }) {
-                changeSelectionToPos(currentSelectionPos.first, 0)
+            if(currentSelectionPos.second == rows.indexOfLast { it != 0 }) { // If the current Button is in the last row
+                changeSelectionToPos(currentSelectionPos.first, 0) // Go back to the first row
             } else {
                 val x = currentSelectionPos.first
                 val y = currentSelectionPos.second + 1
-                if(x > rows[y] - 1) {
+                if(x > rows[y] - 1) { // Same as in up but with down
                     changeSelectionToPos(x - 1, y)
                 } else {
                     changeSelectionToPos(x, y)
@@ -114,10 +114,10 @@ class PokeNav: Screen(TextComponent("PokeNav")) {
         if(pKeyCode == InputConstants.KEY_SPACE) {
             buttons.firstOrNull {
                 it.posX == currentSelectionPos.first && it.posY == currentSelectionPos.second
-            }?.onPress()
+            }?.onPress() // Executes the onPress action for the currently selected button
         }
         if(pKeyCode == PokeNavigatorBinding.key.value || pKeyCode == InputConstants.KEY_LSHIFT || pKeyCode == InputConstants.KEY_RSHIFT) {
-            aboutToClose = true
+            aboutToClose = true // This is needed so the UI doesn't open itself afterwards again (Closing with same key as opening) -> KeyReleased
         }
 
         return super.keyPressed(pKeyCode, pScanCode, pModifiers)
@@ -125,11 +125,14 @@ class PokeNav: Screen(TextComponent("PokeNav")) {
 
     override fun keyReleased(pKeyCode: Int, pScanCode: Int, pModifiers: Int): Boolean {
         if((pKeyCode == PokeNavigatorBinding.key.value || pKeyCode == InputConstants.KEY_LSHIFT || pKeyCode == InputConstants.KEY_RSHIFT) && aboutToClose) {
-            Minecraft.getInstance().setScreen(null)
+            Minecraft.getInstance().setScreen(null) // So we only close if the Key was released
         }
         return super.keyReleased(pKeyCode, pScanCode, pModifiers)
     }
 
+    /**
+     * Change the currentSelection thingy to the position of another Button
+     */
     private fun changeSelectionToPos(posX: Int, posY: Int) {
         val newWidth = getWidthForPos(posX) - 2
         val newHeight = getHeightFor(posY) - 2
@@ -137,14 +140,23 @@ class PokeNav: Screen(TextComponent("PokeNav")) {
         currentSelectionPos = Pair(posX, posY)
     }
 
+    /**
+     * Method for calculating the width based on the background, spacing and button position
+     */
     private fun getWidthForPos(posX: Int): Int {
         return (width - backgroundWidth) / 2 + (posX + 1) * SPACING + (posX) * buttonWidth
     }
 
+    /**
+     * Method for calculating the height based on the background, spacing and button position
+     */
     private fun getHeightFor(posY: Int): Int {
         return (height - backgroundHeight) / 2 + (posY + 1) * SPACING + (posY) * buttonHeight
     }
 
+    /**
+     * To simplify creating PositionAwareImageButtons
+     */
     private fun posImageButtonOf(
         posX: Int,
         posY: Int,
@@ -161,6 +173,10 @@ class PokeNav: Screen(TextComponent("PokeNav")) {
         )
     }
 
+    /**
+     * What should happen on Button press - START
+     */
+
     private fun onPressPokedex(button: Button) {
         println("Pressed Pokedex")
     }
@@ -173,6 +189,13 @@ class PokeNav: Screen(TextComponent("PokeNav")) {
         println("Pressed Question")
     }
 
+    /**
+     * What should happen on Button press - END
+     */
+
+    /**
+     * Rendering the background texture
+     */
     override fun render(pMatrixStack: PoseStack, pMouseX: Int, pMouseY: Int, pPartialTicks: Float) {
         renderBackground(pMatrixStack)
 
@@ -187,6 +210,9 @@ class PokeNav: Screen(TextComponent("PokeNav")) {
         super.render(pMatrixStack, pMouseX, pMouseY, pPartialTicks)
     }
 
+    /**
+     * Whether the screen should pause the game or not
+     */
     override fun isPauseScreen(): Boolean {
         return true
     }
