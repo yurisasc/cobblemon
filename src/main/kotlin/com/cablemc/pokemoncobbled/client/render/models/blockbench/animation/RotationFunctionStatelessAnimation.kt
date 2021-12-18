d3e602cf9bc88c251d0d2e012253a17a5635acdf
Landon <46169669+landonjw@users.noreply.github.com>
2021-12-18 18:10:10 -0500

    Use extension function

--- src/main/kotlin/com/cablemc/pokemoncobbled/client/render/models/blockbench/animation/RotationFunctionStatelessAnimation.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/client/render/models/blockbench/animation/RotationFunctionStatelessAnimation.kt
@@ -2,7 +2,7 @@ package com.cablemc.pokemoncobbled.client.render.models.blockbench.animation
 
 import com.cablemc.pokemoncobbled.client.render.models.blockbench.PoseableEntityModel
 import com.cablemc.pokemoncobbled.client.render.models.blockbench.frame.ModelFrame
-import com.cablemc.pokemoncobbled.client.render.models.blockbench.pose.TransformedModelPart
+import com.cablemc.pokemoncobbled.client.render.models.blockbench.setRotation
 import com.cablemc.pokemoncobbled.client.render.models.blockbench.wavefunction.LineFunction
 import net.minecraft.client.model.geom.ModelPart
 import net.minecraft.world.entity.Entity
@@ -19,16 +19,6 @@ class RotationFunctionStatelessAnimation<T : Entity>(
     override val targetFrame = ModelFrame::class.java
     override fun setAngles(entity: T?, model: PoseableEntityModel<T>, limbSwing: Float, limbSwingAmount: Float, ageInTicks: Float, headYaw: Float, headPitch: Float) {
         val animationTick = entity?.let { model.getState(it).animationTick } ?: 0F
-        when (axis) {
-            TransformedModelPart.X_AXIS -> {
-                part.setRotation(function(animationTick), part.yRot, part.zRot)
-            }
-            TransformedModelPart.Y_AXIS -> {
-                part.setRotation(part.xRot, function(animationTick), part.zRot)
-            }
-            TransformedModelPart.Z_AXIS -> {
-                part.setRotation(part.xRot, part.yRot, function(animationTick))
-            }
-        }
+        part.setRotation(axis, function(animationTick))
     }
 }
