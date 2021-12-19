a6407be8fb0204e3fc734cd326128007c587ea76
Qumoo <me@qumo.at>
2021-12-19 13:56:11 +0100

    proposal for adding move Move functionality

--- src/main/kotlin/com/cablemc/pokemoncobbled/client/gui/summary/widgets/pages/moves/MovesMoveButton.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/client/gui/summary/widgets/pages/moves/MovesMoveButton.kt
@@ -1,13 +1,28 @@
 package com.cablemc.pokemoncobbled.client.gui.summary.widgets.pages.moves
 
+import com.cablemc.pokemoncobbled.common.PokemonCobbled
+import com.mojang.blaze3d.systems.RenderSystem
+import com.mojang.blaze3d.vertex.PoseStack
 import net.minecraft.client.gui.components.Button
-import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.TextComponent
+import net.minecraft.resources.ResourceLocation
 
 class MovesMoveButton(
     pX: Int, pY: Int,
     pWidth: Int, pHeight: Int,
-    private val component: Component,
     onPress: OnPress
-): Button(pX, pY, pWidth, pHeight, component, onPress) {
+): Button(pX, pY, pWidth, pHeight, TextComponent("MoveButton"), onPress) {
 
+    companion object {
+        private val buttonResource = ResourceLocation(PokemonCobbled.MODID, "ui/summary/summary_moves_overlay_swap.png")
+    }
+
+    override fun renderButton(pMatrixStack: PoseStack, pMouseX: Int, pMouseY: Int, pPartialTicks: Float) {
+        RenderSystem.setShaderTexture(0, buttonResource)
+        blit(pMatrixStack, x, y, 0F, 0F, width, height, width, height)
+    }
+
+    override fun onPress() {
+        super.onPress()
+    }
 }
--- src/main/kotlin/com/cablemc/pokemoncobbled/client/gui/summary/widgets/pages/moves/MovesWidget.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/client/gui/summary/widgets/pages/moves/MovesWidget.kt
@@ -11,7 +11,7 @@ import net.minecraft.resources.ResourceLocation
 class MovesWidget(
     pX: Int, pY: Int,
     pWidth: Int, pHeight: Int,
-    summary: Summary
+    private val summary: Summary
 ): SoundlessWidget(pX, pY, pWidth, pHeight, TextComponent("MovesWidget")) {
 
     companion object {
@@ -24,8 +24,8 @@ class MovesWidget(
     private var index = -1
     private val moves = summary.pokemonMoves().filterNotNull().map { move ->
         index++
-        MoveWidget(x + 20, y + 28 + (moveHeight + 3) * index, moveWidth, moveHeight, move, x + 5, y + 165)
-    }.onEach {
+        MoveWidget(x + 19, y + 27 + (moveHeight + 3) * index, moveWidth, moveHeight, move, x + 5, y + 165, width, height, this)
+    }.toMutableList().onEach {
         addWidget(it)
     }
 
@@ -36,6 +36,42 @@ class MovesWidget(
         blit(pMatrixStack, x, y, 0F, 0F, width, height, width, height)
         moves.forEach {
             it.render(pMatrixStack, pMouseX, pMouseY, pPartialTicks)
+        }
+    }
+
+    private fun updateMoves() {
+        moves.forEach {
+            it.y = y + 27 + (moveHeight + 3) * moves.indexOf(it)
+        }
+    }
+
+    fun moveMove(move: MoveWidget, up: Boolean) {
+        println("Begin")
+        for(i in 0..3) {
+            println("$i is ${moves[i].move.name}")
+        }
+        val movePos = moves.indexOfFirst { it == move }
+        println("MovePos $movePos")
+        if(moves.size <= movePos || movePos == -1) {
+            return
+        }
+        var targetSlot: Int
+        println("Moving Move")
+        if(up) {
+            targetSlot = movePos - 1
+            if(targetSlot == -1)
+                targetSlot = moves.size - 1
+        } else {
+            targetSlot = movePos + 1
+            if(targetSlot >= moves.size)
+                targetSlot = 0
+        }
+        val temp = moves[targetSlot]
+        moves.set(targetSlot, moves[movePos])
+        moves.set(movePos, temp)
+        updateMoves()
+        for(i in 0..3) {
+            println("$i is ${moves[i].move.name}")
         }
     }
 
