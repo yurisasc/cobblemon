9c924f883aab64daa104be158cd60224cdff4d8f
Qumoo <me@qumo.at>
2021-12-22 14:59:28 +0100

    added Fonts object and extension function

---
+++ src/main/kotlin/com/cablemc/pokemoncobbled/client/gui/Fonts.kt
@@ -0,0 +1,11 @@
+package com.cablemc.pokemoncobbled.client.gui
+
+import com.cablemc.pokemoncobbled.common.PokemonCobbled
+import net.minecraft.resources.ResourceLocation
+
+object Fonts {
+
+    val OSWALD = ResourceLocation(PokemonCobbled.MODID, "oswald")
+    val OSWALD_SMALL = ResourceLocation(PokemonCobbled.MODID, "oswald_small")
+
+}
--- src/main/kotlin/com/cablemc/pokemoncobbled/client/gui/GuiUtils.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/client/gui/GuiUtils.kt
@@ -3,8 +3,10 @@ package com.cablemc.pokemoncobbled.client.gui
 import com.mojang.blaze3d.platform.GlStateManager
 import com.mojang.blaze3d.systems.RenderSystem
 import com.mojang.blaze3d.vertex.PoseStack
+import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.Gui
 import net.minecraft.client.renderer.GameRenderer
+import net.minecraft.network.chat.TextComponent
 import net.minecraft.resources.ResourceLocation
 
 fun blitk(
@@ -31,4 +33,21 @@ fun blitk(
         width.toInt(), height.toInt(),
         width.toInt(), height.toInt()
     )
+}
+
+fun drawCenteredText(
+    poseStack: PoseStack,
+    font: ResourceLocation,
+    text: TextComponent,
+    x: Number,
+    y: Number,
+    colour: Int,
+    shadow: Boolean = true
+) {
+    val comp = text.withStyle(text.style.withFont(font))
+    val mcFont = Minecraft.getInstance().font
+    if(shadow)
+        mcFont.drawShadow(poseStack, comp, x.toFloat() - mcFont.width(comp) / 2, y.toFloat(), colour)
+    else
+        mcFont.draw(poseStack, comp, x.toFloat() - mcFont.width(comp) / 2, y.toFloat(), colour)
 }
