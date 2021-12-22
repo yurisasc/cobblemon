c6100cf3c2309e6eb9d78171815bd58d15454553
Qumoo <me@qumo.at>
2021-12-22 15:03:27 +0100

    moved offset values to consts draw text via extension function with custom font

--- src/main/kotlin/com/cablemc/pokemoncobbled/client/gui/summary/widgets/pages/moves/MoveWidget.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/client/gui/summary/widgets/pages/moves/MoveWidget.kt
@@ -1,14 +1,14 @@
 package com.cablemc.pokemoncobbled.client.gui.summary.widgets.pages.moves
 
+import com.cablemc.pokemoncobbled.client.gui.Fonts
 import com.cablemc.pokemoncobbled.client.gui.blitk
+import com.cablemc.pokemoncobbled.client.gui.drawCenteredText
 import com.cablemc.pokemoncobbled.client.gui.summary.mock.PokemonMove
 import com.cablemc.pokemoncobbled.client.gui.summary.widgets.SoundlessWidget
 import com.cablemc.pokemoncobbled.client.gui.summary.widgets.type.SingleTypeWidget
 import com.cablemc.pokemoncobbled.common.PokemonCobbled
 import com.mojang.blaze3d.systems.RenderSystem
 import com.mojang.blaze3d.vertex.PoseStack
-import net.minecraft.client.Minecraft
-import net.minecraft.client.gui.GuiComponent
 import net.minecraft.network.chat.TextComponent
 import net.minecraft.resources.ResourceLocation
 import kotlin.math.roundToInt
@@ -30,25 +30,29 @@ class MoveWidget(
         private const val PP_HEIGHT_DIFF = 20
         private const val MOVE_BUTTON_WIDTH = 15
         private const val MOVE_BUTTON_HEIGHT = 12
+        private const val TYPE_WIDGET_Y_OFFSET = 2
+        private const val MOVE_UP_BUTTON_Y_OFFSET = 2
+        private const val MOVE_DOWN_BUTTON_Y_OFFSET = 14
+        private const val MOVE_NAME_COLOUR = 0x0A0A0A
     }
 
-    private val typeWidget = SingleTypeWidget(x + 3, y + 2, 18, 18, move.type)
+    private val typeWidget = SingleTypeWidget(x + 3, y + TYPE_WIDGET_Y_OFFSET, 18, 18, move.type)
     private val moveInfoWidget = MoveInfoWidget(x, y, width, height, move, infoX, infoY)
-    private val moveUpButton = MovesMoveButton(x - 15, y + 2, MOVE_BUTTON_WIDTH, MOVE_BUTTON_HEIGHT) {
-        println("Moving ${move.name}")
+    private val moveUpButton = MovesMoveButton(x - 15, y + MOVE_UP_BUTTON_Y_OFFSET, MOVE_BUTTON_WIDTH, MOVE_BUTTON_HEIGHT, textureWidth, textureHeight, true) {
         movesWidget.moveMove(this, true)
     }.apply {
         addWidget(this)
     }
-    private val moveDownButton = MovesMoveButton(x, y, width, height) {
+    private val moveDownButton = MovesMoveButton(x - 15, y + MOVE_DOWN_BUTTON_Y_OFFSET, MOVE_BUTTON_WIDTH, MOVE_BUTTON_HEIGHT, textureWidth, textureHeight, false) {
         movesWidget.moveMove(this, false)
     }.apply {
         addWidget(this)
     }
 
     fun update() {
-        typeWidget.y = y + 2
-        moveUpButton.y = y + 2
+        typeWidget.y = y + TYPE_WIDGET_Y_OFFSET
+        moveUpButton.y = y + MOVE_UP_BUTTON_Y_OFFSET
+        moveDownButton.y = y + MOVE_DOWN_BUTTON_Y_OFFSET
     }
 
     override fun render(pMatrixStack: PoseStack, pMouseX: Int, pMouseY: Int, pPartialTicks: Float) {
@@ -71,14 +75,17 @@ class MoveWidget(
         // Render remaining PP Text
         pMatrixStack.pushPose()
         pMatrixStack.scale(0.6F, 0.6F, 0.6F)
-        GuiComponent.drawCenteredString(pMatrixStack, Minecraft.getInstance().font, "${move.curPp} / ${move.maxPp}", ((x + width / 2) / 0.6).roundToInt() + 3, ((y + 23) / 0.6).roundToInt() + 1, 0xFFFFFF)
+        drawCenteredText(pMatrixStack, Fonts.OSWALD_SMALL, TextComponent("${move.curPp} / ${move.maxPp}"), ((x + width / 2) / 0.6) + 3, ((y + 23) / 0.6) + 1, 0xFFFFFF)
         pMatrixStack.popPose()
 
         // Render Type Icon
         typeWidget.render(pMatrixStack, pMouseX, pMouseY, pPartialTicks)
 
         // Render Move Name
-        GuiComponent.drawCenteredString(pMatrixStack, Minecraft.getInstance().font, move.name, x + 85, y + 7, 0x5A5A5A)
+        pMatrixStack.pushPose()
+        pMatrixStack.scale(0.75F, 0.75F, 0.75F)
+        drawCenteredText(pMatrixStack, Fonts.OSWALD, TextComponent(move.name), (x + 85) / 0.75F, y / 0.75F + 7, MOVE_NAME_COLOUR, false)
+        pMatrixStack.popPose()
 
         // Render Move Info
         if(isHovered()) {
@@ -87,7 +94,7 @@ class MoveWidget(
 
         // Render Move Move Button
         moveUpButton.render(pMatrixStack, pMouseX, pMouseY, pPartialTicks)
-        //moveDownButton.render(pMatrixStack, pMouseX, pMouseY, pPartialTicks)
+        moveDownButton.render(pMatrixStack, pMouseX, pMouseY, pPartialTicks)
     }
 
     // Get the remaining PP as percentage
