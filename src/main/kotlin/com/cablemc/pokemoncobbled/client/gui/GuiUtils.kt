316ea1e44ca22707bde5d73ef8b8bf19204be9c0
Qumoo <me@qumo.at>
2021-12-23 11:33:32 +0100

    added drawCenteredText method to GuiUtils

--- src/main/kotlin/com/cablemc/pokemoncobbled/client/gui/GuiUtils.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/client/gui/GuiUtils.kt
@@ -6,7 +6,8 @@ import com.mojang.blaze3d.vertex.PoseStack
 import net.minecraft.client.Minecraft
 import net.minecraft.client.gui.Gui
 import net.minecraft.client.renderer.GameRenderer
-import net.minecraft.network.chat.TextComponent
+import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.MutableComponent
 import net.minecraft.resources.ResourceLocation
 
 fun blitk(
@@ -38,13 +39,13 @@ fun blitk(
 fun drawCenteredText(
     poseStack: PoseStack,
     font: ResourceLocation,
-    text: TextComponent,
+    text: Component,
     x: Number,
     y: Number,
     colour: Int,
     shadow: Boolean = true
 ) {
-    val comp = text.withStyle(text.style.withFont(font))
+    val comp = (text as MutableComponent).withStyle(text.style.withFont(font))
     val mcFont = Minecraft.getInstance().font
     if(shadow)
         mcFont.drawShadow(poseStack, comp, x.toFloat() - mcFont.width(comp) / 2, y.toFloat(), colour)
