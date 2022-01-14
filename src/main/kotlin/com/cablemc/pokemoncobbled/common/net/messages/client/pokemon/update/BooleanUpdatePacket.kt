91e42500eab7f0e7337c968a268c752338b4a347
Deltric <d3ltr1c@gmail.com>
2022-01-13 20:00:44 -0600

    I forgot that its a new year :skull:

--- src/main/kotlin/com/cablemc/pokemoncobbled/common/net/messages/client/pokemon/update/BooleanUpdatePacket.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/common/net/messages/client/pokemon/update/BooleanUpdatePacket.kt
@@ -6,7 +6,7 @@ import net.minecraft.network.FriendlyByteBuf
  * A specific type of update for a Pokémon which updates a single boolean value
  *
  * @author Deltric
- * @since January 13th, 2021
+ * @since January 13th, 2022
  */
 abstract class BooleanUpdatePacket : SingleUpdatePacket<Boolean>(false) {
     override fun encodeValue(buffer: FriendlyByteBuf, value: Boolean) {
