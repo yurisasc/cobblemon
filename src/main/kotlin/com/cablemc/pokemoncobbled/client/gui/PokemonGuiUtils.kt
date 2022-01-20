56525ff3dba6ad8d676b18885109465766006da9
Qumoo <me@qumo.at>
2022-01-20 17:49:35 +0100

    drawPokemon from Hiro's polished party branch (and some adjustments)

--- src/main/kotlin/com/cablemc/pokemoncobbled/client/gui/PokemonGuiUtils.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/client/gui/PokemonGuiUtils.kt
@@ -8,6 +8,7 @@ import com.mojang.blaze3d.platform.Lighting
 import com.mojang.blaze3d.systems.RenderSystem
 import com.mojang.blaze3d.vertex.PoseStack
 import com.mojang.math.Quaternion
+import com.mojang.math.Vector3f
 import net.minecraft.client.Minecraft
 import net.minecraft.client.renderer.LightTexture
 import net.minecraft.client.renderer.texture.OverlayTexture
@@ -53,3 +54,36 @@ fun drawProfilePokemon(
     Lighting.setupFor3DItems()
 }
 
+fun drawPokemon(pokemon: Pokemon, poseStack: PoseStack) {
+    val model = PokemonModelRepository.getModel(pokemon).entityModel
+    val texture = PokemonModelRepository.getModelTexture(pokemon)
+
+    val renderType = model.renderType(texture)
+
+    val scale = 60F
+    RenderSystem.applyModelViewMatrix()
+    poseStack.scale(scale, scale, -scale)
+    val quaternion1 = Vector3f.YP.rotationDegrees(32F)
+
+    if (model is PokemonPoseableModel) {
+        model.setupAnimStateless(PoseType.PROFILE)
+        poseStack.translate(model.profileTranslation.x, model.profileTranslation.y, model.profileTranslation.z)
+        poseStack.scale(model.profileScale, model.profileScale, model.profileScale)
+    }
+
+    poseStack.mulPose(quaternion1)
+    Lighting.setupForEntityInInventory()
+    val entityRenderDispatcher = Minecraft.getInstance().entityRenderDispatcher
+    quaternion1.conj()
+    entityRenderDispatcher.overrideCameraOrientation(quaternion1)
+    entityRenderDispatcher.setRenderShadow(false)
+
+    val bufferSource = Minecraft.getInstance().renderBuffers().bufferSource()
+    val buffer = bufferSource.getBuffer(renderType)
+
+    val packedLight = LightTexture.pack(15, 15)
+    model.renderToBuffer(poseStack, buffer, packedLight, OverlayTexture.NO_OVERLAY, 1F, 1F, 1F, 1F)
+    bufferSource.endBatch()
+    entityRenderDispatcher.setRenderShadow(true)
+    Lighting.setupFor3DItems()
+}
