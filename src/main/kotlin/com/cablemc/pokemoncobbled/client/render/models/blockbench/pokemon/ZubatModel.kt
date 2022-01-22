ba0b1725a45858e2c2e179ab519a7a8dd016ff94
Paul <paul_l_preston@hotmail.com>
2022-01-22 23:10:05 +1000

    Update Zubat model and texture to apply inflation fix for wings

--- src/main/kotlin/com/cablemc/pokemoncobbled/client/render/models/blockbench/pokemon/ZubatModel.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/client/render/models/blockbench/pokemon/ZubatModel.kt
@@ -23,6 +23,7 @@ import net.minecraft.client.model.geom.builders.LayerDefinition
 import net.minecraft.client.model.geom.builders.MeshDefinition
 import net.minecraft.util.Mth.PI
 
+
 class ZubatModel(root: ModelPart) : PokemonPoseableModel(), BiWingedFrame, EaredFrame {
     override val rootPart: ModelPart = registerRelevantPart("zubat", root.getChild("zubat"))
     override val leftWing = rootPart.getChildOf("body", "leftwing")
@@ -110,53 +111,62 @@ class ZubatModel(root: ModelPart) : PokemonPoseableModel(), BiWingedFrame, Eared
         
         val LAYER_LOCATION = ModelLayerLocation(cobbledResource("zubat"), "main")
         fun createBodyLayer(): LayerDefinition {
-            val meshdefinition = MeshDefinition()
-            val partdefinition = meshdefinition.root
-            val zubat = partdefinition.addOrReplaceChild("zubat", CubeListBuilder.create(), PartPose.offset(0.0f, 24.0f, 0.0f))
+            val meshDefinition = MeshDefinition()
+            val partDefinition = meshDefinition.root
+
+            val zubat = partDefinition.addOrReplaceChild("zubat", CubeListBuilder.create(), PartPose.offset(0.0f, 24.0f, 0.0f))
+
             val body = zubat.addOrReplaceChild(
                 "body",
                 CubeListBuilder.create().texOffs(0, 16)
                     .addBox(-2.5f, -8.0f, -1.5f, 5.0f, 7.0f, 3.0f, CubeDeformation(0.0f))
-                    .texOffs(24, 29).addBox(-2.0f, -7.5f, -1.55f, 4.0f, 3.0f, 0.0f, CubeDeformation(0.0f)),
+                    .texOffs(24, 29).addBox(-2.0f, -7.5f, -1.55f, 4.0f, 3.0f, 0.0f, CubeDeformation(0.02f)),
                 PartPose.offset(0.0f, -7.0f, 0.0f)
             )
+
             val cube_r1 = body.addOrReplaceChild(
                 "cube_r1",
                 CubeListBuilder.create().texOffs(22, 7)
-                    .addBox(-0.5f, -3.5f, 0.0f, 1.0f, 7.0f, 0.0f, CubeDeformation(0.0f)),
+                    .addBox(-0.5f, -3.5f, 0.0f, 1.0f, 7.0f, 0.0f, CubeDeformation(0.02f)),
                 PartPose.offsetAndRotation(1.75f, 2.25f, 0.0f, 0.0f, 0.0f, -0.2618f)
             )
+
             val cube_r2 = body.addOrReplaceChild(
                 "cube_r2",
                 CubeListBuilder.create().texOffs(22, 0)
-                    .addBox(-0.5f, -3.5f, 0.0f, 1.0f, 7.0f, 0.0f, CubeDeformation(0.0f)),
+                    .addBox(-0.5f, -3.5f, 0.0f, 1.0f, 7.0f, 0.0f, CubeDeformation(0.02f)),
                 PartPose.offsetAndRotation(-1.75f, 2.25f, 0.0f, 0.0f, 0.0f, 0.2618f)
             )
+
             val rightear = body.addOrReplaceChild(
                 "rightear",
                 CubeListBuilder.create().texOffs(16, 20)
                     .addBox(-1.5f, -4.0f, 0.0f, 3.0f, 4.0f, 0.0f, CubeDeformation(0.02f)),
                 PartPose.offsetAndRotation(-1.5f, -7.25f, 0.0f, 0.0f, 0.0f, -0.6981f)
             )
+
             val leftear = body.addOrReplaceChild(
                 "leftear",
                 CubeListBuilder.create().texOffs(16, 16)
                     .addBox(-1.5f, -4.0f, 0.0f, 3.0f, 4.0f, 0.0f, CubeDeformation(0.02f)),
                 PartPose.offsetAndRotation(1.5f, -7.25f, 0.0f, 0.0f, 0.0f, 0.6981f)
             )
+
             val rightwing = body.addOrReplaceChild(
                 "rightwing",
                 CubeListBuilder.create().texOffs(0, 8)
-                    .addBox(-11.0f, -6.0f, 0.0f, 11.0f, 8.0f, 0.0f, CubeDeformation(0.0f)),
+                    .addBox(-11.0f, -6.0f, 0.0f, 11.0f, 8.0f, 0.0f, CubeDeformation(0.02f)),
                 PartPose.offset(-2.5f, -3.0f, 0.0f)
             )
+
             val leftwing = body.addOrReplaceChild(
                 "leftwing",
                 CubeListBuilder.create().texOffs(0, 0)
-                    .addBox(0.0f, -6.0f, 0.0f, 11.0f, 8.0f, 0.0f, CubeDeformation(0.0f)),
+                    .addBox(0.0f, -6.0f, 0.0f, 11.0f, 8.0f, 0.0f, CubeDeformation(0.02f)),
                 PartPose.offset(2.5f, -3.0f, 0.0f)
             )
-            return LayerDefinition.create(meshdefinition, 32, 32)
+
+            return LayerDefinition.create(meshDefinition, 32, 32)
         }
     }
 }
