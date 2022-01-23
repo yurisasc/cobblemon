04e774210c4c6f078bbcd7a69cef69e31e14520e
Deltric <d3ltr1c@gmail.com>
2022-01-22 23:14:30 -0600

    Update CommandRegistrar.kt

--- src/main/kotlin/com/cablemc/pokemoncobbled/common/CommandRegistrar.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/common/CommandRegistrar.kt
@@ -1,12 +1,9 @@
 package com.cablemc.pokemoncobbled.common
 
-import com.cablemc.pokemoncobbled.common.command.ShowdownWriteCommand
 import com.cablemc.pokemoncobbled.common.command.GivePokemon
 import com.cablemc.pokemoncobbled.common.command.SpawnPokemon
-import com.cablemc.pokemoncobbled.common.command.ShowdownReadCommand
 import com.cablemc.pokemoncobbled.common.command.ChangeScaleAndSize
 import com.cablemc.pokemoncobbled.common.command.TestCommand
-import com.cablemc.pokemoncobbled.common.command.*
 import net.minecraftforge.event.RegisterCommandsEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 
