896818676ca5464902bfeae39e1b42e685da6060
Qumoo <me@qumo.at>
2022-01-26 16:14:35 +0100

    add first testing ShoulderEffect (LightSourceEffect)

---
+++ src/main/kotlin/com/cablemc/pokemoncobbled/common/api/pokemon/effect/LightSourceEffect.kt
@@ -0,0 +1,17 @@
+package com.cablemc.pokemoncobbled.common.api.pokemon.effect
+
+import com.cablemc.pokemoncobbled.common.entity.pokemon.PokemonEntity
+import net.minecraft.server.level.ServerPlayer
+
+object LightSourceEffect: ShoulderEffect() {
+    override val name: String
+        get() = "light_source"
+
+    override fun applyEffect(pokemonEntity: PokemonEntity, player: ServerPlayer) {
+        println("Applying effect...")
+    }
+
+    override fun removeEffect(pokemonEntity: PokemonEntity, player: ServerPlayer) {
+        println("Removing effect...")
+    }
+}
