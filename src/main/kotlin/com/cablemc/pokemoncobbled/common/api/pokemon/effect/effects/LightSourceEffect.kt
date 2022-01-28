c4a31b8ec7ba85b8ade20ea80263f15611965cba
Qumoo <me@qumo.at>
2022-01-29 00:09:45 +0100

    Add serialization to LightSourceEffect

--- src/main/kotlin/com/cablemc/pokemoncobbled/common/api/pokemon/effect/effects/LightSourceEffect.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/common/api/pokemon/effect/effects/LightSourceEffect.kt
@@ -2,17 +2,29 @@ package com.cablemc.pokemoncobbled.common.api.pokemon.effect.effects
 
 import com.cablemc.pokemoncobbled.common.api.pokemon.effect.ShoulderEffect
 import com.cablemc.pokemoncobbled.common.entity.pokemon.PokemonEntity
+import com.google.gson.JsonObject
 import net.minecraft.server.level.ServerPlayer
 
-object LightSourceEffect: ShoulderEffect() {
-    override val name: String
-        get() = "light_source"
+object LightSourceEffect: ShoulderEffect {
+    override val name = "light_source"
+
+    private var test = "AAA"
 
     override fun applyEffect(pokemonEntity: PokemonEntity?, player: ServerPlayer) {
-        println("Applying effect...")
+        println("Applying effect... $test")
     }
 
     override fun removeEffect(pokemonEntity: PokemonEntity?, player: ServerPlayer) {
         println("Removing effect...")
+    }
+
+    override fun serialize(json: JsonObject): JsonObject {
+        json.addProperty("Test", test)
+        return json
+    }
+
+    override fun deserialize(json: JsonObject): ShoulderEffect {
+        test = json.get("Test").asString
+        return this
     }
 }
