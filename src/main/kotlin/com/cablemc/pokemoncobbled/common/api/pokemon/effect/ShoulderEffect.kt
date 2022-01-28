c2a798447da7bf4c6e23a8c9012b9565b822df01
Qumoo <me@qumo.at>
2022-01-29 00:08:45 +0100

    Made ShoulderEffect an Interface

--- src/main/kotlin/com/cablemc/pokemoncobbled/common/api/pokemon/effect/ShoulderEffect.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/common/api/pokemon/effect/ShoulderEffect.kt
@@ -1,17 +1,21 @@
 package com.cablemc.pokemoncobbled.common.api.pokemon.effect
 
 import com.cablemc.pokemoncobbled.common.entity.pokemon.PokemonEntity
+import com.google.gson.JsonObject
 import net.minecraft.server.level.ServerPlayer
 
 /**
- * Base class for all ShoulderEffects
+ * Interface for all ShoulderEffects
  *
  * @author Qu
  * @since 2022-01-26
  */
-abstract class ShoulderEffect {
-    abstract val name: String
+interface ShoulderEffect {
+    val name: String
 
-    abstract fun applyEffect(pokemonEntity: PokemonEntity?, player: ServerPlayer)
-    abstract fun removeEffect(pokemonEntity: PokemonEntity?, player: ServerPlayer)
+    fun applyEffect(pokemonEntity: PokemonEntity?, player: ServerPlayer)
+    fun removeEffect(pokemonEntity: PokemonEntity?, player: ServerPlayer)
+
+    fun serialize(json: JsonObject): JsonObject
+    fun deserialize(json: JsonObject): ShoulderEffect
 }
