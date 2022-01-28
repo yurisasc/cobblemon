b897b53281a07e4d88652d3986e1b2f48c670564
Qumoo <me@qumo.at>
2022-01-29 00:10:50 +0100

    Only load type and then pass to Effect to do the remaining serialization

--- src/main/kotlin/com/cablemc/pokemoncobbled/common/api/pokemon/effect/adapter/ShoulderEffectAdapter.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/common/api/pokemon/effect/adapter/ShoulderEffectAdapter.kt
@@ -5,17 +5,20 @@ import com.cablemc.pokemoncobbled.common.api.pokemon.effect.ShoulderEffectRegist
 import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
-import com.google.gson.JsonPrimitive
+import com.google.gson.JsonObject
 import com.google.gson.JsonSerializationContext
 import com.google.gson.JsonSerializer
 import java.lang.reflect.Type
 
 object ShoulderEffectAdapter: JsonDeserializer<ShoulderEffect>, JsonSerializer<ShoulderEffect> {
     override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): ShoulderEffect {
-        return ShoulderEffectRegistry.get(json.asString) ?: throw NoSuchElementException("Error reading ShouldEffect ${json.asString}")
+        return ShoulderEffectRegistry.get(json.asJsonObject.get("type").asString)?.also { it.deserialize(json.asJsonObject) }
+            ?: throw NoSuchElementException("Error reading ShouldEffect ${json.asString}")
     }
 
     override fun serialize(src: ShoulderEffect, typeOfSrc: Type, context: JsonSerializationContext): JsonElement {
-        return JsonPrimitive(src.name)
+        val json = JsonObject()
+        json.addProperty("type", src.name)
+        return src.serialize(json)
     }
 }
