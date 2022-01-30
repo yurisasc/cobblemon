677a76264a6e2615ea5f4549802b6478b208185e
Qumoo <me@qumo.at>
2022-01-30 22:08:50 +0100

    Added SlowFallEffect#onFallEnd

--- src/main/kotlin/com/cablemc/pokemoncobbled/common/pokemon/effects/SlowFallEffect.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/common/pokemon/effects/SlowFallEffect.kt
@@ -10,6 +10,13 @@ import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.event.entity.living.LivingEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 
+/**
+ * Effect that allows for slow falling after [slowAfter] blocks.
+ * The value for [slowAfter] can be set per Form in the Species JSON.
+ *
+ * @author Qu
+ * @since 2022-01-29
+ */
 class SlowFallEffect: ShoulderEffect {
     companion object {
         init {
@@ -18,6 +25,7 @@ class SlowFallEffect: ShoulderEffect {
 
         private const val SLOW_AFTER_PROPERTY = "slowAfter"
         private val observeMap = mutableMapOf<ServerPlayer, SlowFallEffect>()
+        private val isFalling = mutableSetOf<ServerPlayer>()
 
         @SubscribeEvent
         fun onLivingUpdate(event: LivingEvent.LivingUpdateEvent) {
@@ -26,9 +34,13 @@ class SlowFallEffect: ShoulderEffect {
             if (!observeMap.containsKey(player)) return
 
             if (player.fallDistance > 0) observeMap[player]?.onFall(player)
+            if (isFalling.contains(player) && player.fallDistance == 0F) observeMap[player]?.onFallEnd(player)
         }
     }
 
+    /**
+     * Amount of Blocks the [ServerPlayer] needs to fall to trigger the [SlowFallEffect]
+     */
     private var slowAfter = 5
 
     override fun applyEffect(pokemonEntity: PokemonEntity?, player: ServerPlayer, isLeft: Boolean) {
@@ -37,12 +49,24 @@ class SlowFallEffect: ShoulderEffect {
 
     override fun removeEffect(pokemonEntity: PokemonEntity?, player: ServerPlayer, isLeft: Boolean) {
         observeMap.remove(player)
+        isFalling.remove(player)
     }
 
+    /**
+     * Triggers if the [ServerPlayer] is falling
+     */
     fun onFall(player: ServerPlayer) {
         if (player.fallDistance >= slowAfter) {
+            isFalling.add(player)
             player.addEffect(MobEffectInstance(MobEffects.SLOW_FALLING, 60))
         }
+    }
+
+    /**
+     * Triggers when the [ServerPlayer] finished falling
+     */
+    fun onFallEnd(player: ServerPlayer) {
+        isFalling.remove(player)
     }
 
     override fun serialize(json: JsonObject): JsonObject {
