d6454ad3193baf8ce05bfc4dccb9b7097a8b583a
Qumoo <me@qumo.at>
2022-02-02 00:30:13 +0100

    SlowFall Potion effects resets the fall distance => Change to manually applying a different gravity

--- src/main/kotlin/com/cablemc/pokemoncobbled/common/pokemon/effects/SlowFallEffect.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/common/pokemon/effects/SlowFallEffect.kt
@@ -1,14 +1,16 @@
 package com.cablemc.pokemoncobbled.common.pokemon.effects
 
 import com.cablemc.pokemoncobbled.common.api.pokemon.effect.ShoulderEffect
-import com.cablemc.pokemoncobbled.common.entity.pokemon.PokemonEntity
+import com.cablemc.pokemoncobbled.common.pokemon.Pokemon
 import com.google.gson.JsonObject
 import net.minecraft.server.level.ServerPlayer
-import net.minecraft.world.effect.MobEffectInstance
-import net.minecraft.world.effect.MobEffects
+import net.minecraft.world.entity.ai.attributes.AttributeModifier
+import net.minecraftforge.common.ForgeMod
 import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.event.entity.living.LivingEvent
+import net.minecraftforge.event.entity.living.LivingFallEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
+import java.util.UUID
 
 /**
  * Effect that allows for slow falling after [slowAfter] blocks.
@@ -22,19 +24,36 @@ class SlowFallEffect: ShoulderEffect {
         init {
             MinecraftForge.EVENT_BUS.register(this)
         }
+        private val SLOW_FALLING_ID = UUID.fromString("A5B6CF2A-2F7C-31EF-9022-7C3E7D5E6ABB")
+        private val SLOW_FALLING = AttributeModifier(
+            SLOW_FALLING_ID,
+            "Slow falling acceleration reduction",
+            -0.07,
+            AttributeModifier.Operation.ADDITION
+        ) // Add -0.07 to 0.08 so we get the vanilla default of 0.01
 
         private const val SLOW_AFTER_PROPERTY = "slowAfter"
         private val observeMap = mutableMapOf<ServerPlayer, SlowFallEffect>()
-        private val isFalling = mutableSetOf<ServerPlayer>()
 
         @SubscribeEvent
         fun onLivingUpdate(event: LivingEvent.LivingUpdateEvent) {
             if (event.entity !is ServerPlayer) return
             val player = event.entity as ServerPlayer
-            if (!observeMap.containsKey(player)) return
+            if (player !in observeMap) return
+
+            if (!player.gameMode.isSurvival) return
 
             if (player.fallDistance > 0) observeMap[player]?.onFall(player)
-            if (isFalling.contains(player) && player.fallDistance == 0F) observeMap[player]?.onFallEnd(player)
+        }
+
+        @SubscribeEvent
+        fun onFallEnd(event: LivingFallEvent) {
+            if (event.entity !is ServerPlayer) return
+            val player = event.entity as ServerPlayer
+            if (player !in observeMap) return
+
+            event.damageMultiplier = 0.0F
+            observeMap[player]?.onFallEnd(player)
         }
     }
 
@@ -43,13 +62,13 @@ class SlowFallEffect: ShoulderEffect {
      */
     private var slowAfter = 5
 
-    override fun applyEffect(pokemonEntity: PokemonEntity?, player: ServerPlayer, isLeft: Boolean) {
+    override fun applyEffect(pokemon: Pokemon, player: ServerPlayer, isLeft: Boolean) {
         observeMap[player] = this
     }
 
-    override fun removeEffect(pokemonEntity: PokemonEntity?, player: ServerPlayer, isLeft: Boolean) {
+    override fun removeEffect(pokemon: Pokemon, player: ServerPlayer, isLeft: Boolean) {
         observeMap.remove(player)
-        isFalling.remove(player)
+        removeEffect(player)
     }
 
     /**
@@ -57,8 +76,7 @@ class SlowFallEffect: ShoulderEffect {
      */
     fun onFall(player: ServerPlayer) {
         if (player.fallDistance >= slowAfter) {
-            isFalling.add(player)
-            player.addEffect(MobEffectInstance(MobEffects.SLOW_FALLING, 60))
+            addEffect(player)
         }
     }
 
@@ -66,7 +84,20 @@ class SlowFallEffect: ShoulderEffect {
      * Triggers when the [ServerPlayer] finished falling
      */
     fun onFallEnd(player: ServerPlayer) {
-        isFalling.remove(player)
+        player.resetFallDistance()
+        removeEffect(player)
+    }
+
+    private fun addEffect(player: ServerPlayer) {
+        player.getAttribute(ForgeMod.ENTITY_GRAVITY.get())?.let {
+            if (!it.hasModifier(SLOW_FALLING)) it.addTransientModifier(SLOW_FALLING)
+        }
+    }
+
+    private fun removeEffect(player: ServerPlayer) {
+        player.getAttribute(ForgeMod.ENTITY_GRAVITY.get())?.let {
+            if (it.hasModifier(SLOW_FALLING)) it.removeModifier(SLOW_FALLING)
+        }
     }
 
     override fun serialize(json: JsonObject): JsonObject {
