32281e00dfa4d8c94405ff1a7a9c2d154aa78bb4
Qumoo <me@qumo.at>
2022-02-02 00:28:52 +0100

    Remove effects on player leave and apply them on join if the Pokemon is still ShoulderMounted

--- src/main/kotlin/com/cablemc/pokemoncobbled/common/api/pokemon/effect/ShoulderEffectRegistry.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/common/api/pokemon/effect/ShoulderEffectRegistry.kt
@@ -1,7 +1,13 @@
 package com.cablemc.pokemoncobbled.common.api.pokemon.effect
 
+import com.cablemc.pokemoncobbled.common.pokemon.activestate.ShoulderedState
 import com.cablemc.pokemoncobbled.common.pokemon.effects.LightSourceEffect
 import com.cablemc.pokemoncobbled.common.pokemon.effects.SlowFallEffect
+import com.cablemc.pokemoncobbled.common.util.party
+import net.minecraft.server.level.ServerPlayer
+import net.minecraftforge.common.MinecraftForge
+import net.minecraftforge.event.entity.player.PlayerEvent
+import net.minecraftforge.eventbus.api.SubscribeEvent
 
 /**
  * Registry object for ShoulderEffects
@@ -10,6 +16,9 @@ import com.cablemc.pokemoncobbled.common.pokemon.effects.SlowFallEffect
  * @since 2022-01-26
  */
 object ShoulderEffectRegistry {
+    init {
+        MinecraftForge.EVENT_BUS.register(this)
+    }
     private val effects = mutableMapOf<String, Class<out ShoulderEffect>>()
 
     // Effects - START
@@ -17,19 +26,39 @@ object ShoulderEffectRegistry {
     val SLOW_FALL = register("slow_fall", SlowFallEffect::class.java)
     // Effects - END
 
-    fun register(name: String, effect: Class<out ShoulderEffect>): Class<out ShoulderEffect> {
-        return effect.also {
-            effects[name] = it
-        }
-    }
+    fun register(name: String, effect: Class<out ShoulderEffect>) = effect.also { effects[name] = it }
 
     fun unregister(name: String) = effects.remove(name)
 
-    fun getName(clazz: Class<out ShoulderEffect>): String {
-        return effects.firstNotNullOf { if (it.value == clazz) it.key else null }
+    fun getName(clazz: Class<out ShoulderEffect>) = effects.firstNotNullOf { if (it.value == clazz) it.key else null }
+
+    fun get(name: String): Class<out ShoulderEffect>? = effects[name]
+
+    @SubscribeEvent
+    fun onPlayerJoin(event: PlayerEvent.PlayerLoggedInEvent) {
+        val player = event.player as ServerPlayer
+        player.party().filter { it.state is ShoulderedState }.forEach { pkm ->
+            pkm.form.shoulderEffect.forEach {
+                it.applyEffect(
+                    pokemon = pkm,
+                    player = player,
+                    isLeft = (pkm.state as ShoulderedState).isLeftShoulder
+                )
+            }
+        }
     }
 
-    fun get(name: String): Class<out ShoulderEffect>? {
-        return effects[name]
+    @SubscribeEvent
+    fun onPlayerLeave(event: PlayerEvent.PlayerLoggedOutEvent) {
+        val player = event.player as ServerPlayer
+        player.party().filter { it.state is ShoulderedState }.forEach { pkm ->
+            pkm.form.shoulderEffect.forEach {
+                it.removeEffect(
+                    pokemon = pkm,
+                    player = player,
+                    isLeft = (pkm.state as ShoulderedState).isLeftShoulder
+                )
+            }
+        }
     }
 }
