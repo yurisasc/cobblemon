eac3f7bfde38df28c3d69f5ced24a1da5910f93a
Qumoo <me@qumo.at>
2022-02-02 00:27:06 +0100

    Change to Pokemon instead of PokemonEntity

--- src/main/kotlin/com/cablemc/pokemoncobbled/common/api/pokemon/effect/ShoulderEffect.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/common/api/pokemon/effect/ShoulderEffect.kt
@@ -1,6 +1,6 @@
 package com.cablemc.pokemoncobbled.common.api.pokemon.effect
 
-import com.cablemc.pokemoncobbled.common.entity.pokemon.PokemonEntity
+import com.cablemc.pokemoncobbled.common.pokemon.Pokemon
 import com.google.gson.JsonObject
 import net.minecraft.server.level.ServerPlayer
 
@@ -11,8 +11,8 @@ import net.minecraft.server.level.ServerPlayer
  * @since 2022-01-26
  */
 interface ShoulderEffect {
-    fun applyEffect(pokemonEntity: PokemonEntity?, player: ServerPlayer, isLeft: Boolean)
-    fun removeEffect(pokemonEntity: PokemonEntity?, player: ServerPlayer, isLeft: Boolean)
+    fun applyEffect(pokemon: Pokemon, player: ServerPlayer, isLeft: Boolean)
+    fun removeEffect(pokemon: Pokemon, player: ServerPlayer, isLeft: Boolean)
 
     fun serialize(json: JsonObject): JsonObject
     fun deserialize(json: JsonObject): ShoulderEffect
