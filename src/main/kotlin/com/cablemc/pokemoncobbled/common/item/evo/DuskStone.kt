f5fa9f5ea9cbddda1e5c066d1a9b662848164a14
Qumoo <me@qumo.at>
2022-02-13 20:18:09 +0100

    add evolution stone item classes

--- src/main/kotlin/com/cablemc/pokemoncobbled/common/item/evo/DuskStone.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/common/item/evo/DuskStone.kt
@@ -2,6 +2,6 @@ package com.cablemc.pokemoncobbled.common.item.evo
 
 import com.cablemc.pokemoncobbled.common.item.EvolutionStoneItem
 
-class FireStone : EvolutionStoneItem() {
+class DuskStone : EvolutionStoneItem() {
     // TODO: Evolutions on whatever right click idk...
 }
