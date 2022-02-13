1173d8666328f64811897f47cbadac1fa6380415
Qumoo <me@qumo.at>
2022-02-13 20:18:54 +0100

    Use optional parameter instead of 2nd constructor and use Material.STONE not Blocks.STONE

--- src/main/kotlin/com/cablemc/pokemoncobbled/common/api/blocks/EvolutionStoneOre.kt
+++ src/main/kotlin/com/cablemc/pokemoncobbled/common/api/blocks/EvolutionStoneOre.kt
@@ -4,29 +4,23 @@ import net.minecraft.core.BlockPos
 import net.minecraft.util.valueproviders.UniformInt
 import net.minecraft.world.level.LevelReader
 import net.minecraft.world.level.block.Block
-import net.minecraft.world.level.block.Blocks
 import net.minecraft.world.level.block.SoundType
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.material.Material
 
-open class EvolutionStoneOre(properties: Properties) : Block(properties) {
+open class EvolutionStoneOre(properties: Properties, var xpRange: UniformInt = UniformInt.of(1, 2)) : Block(properties) {
     companion object {
         const val NORMAL_DESTROY_TIME = 3.0F
         const val DEEPSLATE_DESTROY_TIME = 4.5F
         const val EXPLOSION_RESISTANCE = 3.0F
-        val NORMAL_PROPERTIES: Properties = Properties.copy(Blocks.STONE)
+        val NORMAL_PROPERTIES: Properties = Properties.of(Material.STONE)
             .requiresCorrectToolForDrops()
             .strength(NORMAL_DESTROY_TIME, EXPLOSION_RESISTANCE)
-        val DEEPSLATE_PROPERTIES: Properties = Properties.copy(Blocks.STONE)
+        val DEEPSLATE_PROPERTIES: Properties = Properties.of(Material.STONE)
             .requiresCorrectToolForDrops()
             .strength(DEEPSLATE_DESTROY_TIME, EXPLOSION_RESISTANCE)
             .sound(SoundType.DEEPSLATE)
     }
-
-    constructor(properties: Properties, xpRange: UniformInt) : this(properties) {
-        this.xpRange = xpRange
-    }
-
-    var xpRange = UniformInt.of(0, 0)
 
     override fun getExpDrop(
         state: BlockState?,
