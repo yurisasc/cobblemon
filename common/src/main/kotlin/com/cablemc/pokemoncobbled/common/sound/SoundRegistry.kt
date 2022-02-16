acb70b01aec6b12a9e674c672ea83ef2194e2f18
Paul <paul_l_preston@hotmail.com>
2022-02-16 21:46:52 +1000

    First run on setting up proper subprojects for Forge & Fabric, issues with Kotlin plugin and mixins (probably)

--- common/src/main/kotlin/com/cablemc/pokemoncobbled/common/sound/SoundRegistry.kt
+++ common/src/main/kotlin/com/cablemc/pokemoncobbled/common/sound/SoundRegistry.kt
@@ -3,10 +3,6 @@ package com.cablemc.pokemoncobbled.common.sound
 import com.cablemc.pokemoncobbled.common.PokemonCobbled
 import com.cablemc.pokemoncobbled.common.util.cobbledResource
 import net.minecraft.sounds.SoundEvent
-import net.minecraftforge.eventbus.api.IEventBus
-import net.minecraftforge.registries.DeferredRegister
-import net.minecraftforge.registries.ForgeRegistries
-import net.minecraftforge.registries.RegistryObject
 
 object SoundRegistry {
     private val SOUNDS = DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, PokemonCobbled.MODID)
