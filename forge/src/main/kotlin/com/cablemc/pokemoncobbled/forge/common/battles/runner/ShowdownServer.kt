72c6b0a6d23d56ac8a21078e9edec4b50cdb6347
Qumoo <me@qumo.at>
2022-02-17 11:28:49 +0100

    now that stdlib is back, ShowdownServer is back to old state

--- forge/src/main/kotlin/com/cablemc/pokemoncobbled/forge/common/battles/runner/ShowdownServer.kt
+++ forge/src/main/kotlin/com/cablemc/pokemoncobbled/forge/common/battles/runner/ShowdownServer.kt
@@ -3,14 +3,14 @@ package com.cablemc.pokemoncobbled.forge.common.battles.runner
 import com.caoccao.javet.interop.V8Host
 import com.caoccao.javet.interop.V8Runtime
 import com.caoccao.javet.values.V8Value
-import java.nio.file.Path
+import kotlin.io.path.Path
 
 object ShowdownServer {
 
     @JvmStatic
     fun main(args: Array<String>) {
         val runtime = V8Host.getNodeInstance().createV8Runtime<V8Runtime>()
-        runtime.let { it.getExecutor(Path.of(args[0])).execute<V8Value>().close() }
+        runtime.use { it.getExecutor(Path(args[0])).execute<V8Value>().close() }
     }
 
 }
