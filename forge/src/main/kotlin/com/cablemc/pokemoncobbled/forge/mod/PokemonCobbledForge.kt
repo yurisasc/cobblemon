1c51ee35f9c6b1c5846958e470ac13ce9856a914
Qumoo <me@qumo.at>
2022-02-23 00:49:46 +0100

    Separate Client and Common setups

--- forge/src/main/kotlin/com/cablemc/pokemoncobbled/forge/mod/PokemonCobbledForge.kt
+++ forge/src/main/kotlin/com/cablemc/pokemoncobbled/forge/mod/PokemonCobbledForge.kt
@@ -8,18 +8,15 @@ import com.cablemc.pokemoncobbled.common.PokemonCobbledModImplementation
 import com.cablemc.pokemoncobbled.common.api.events.CobbledEvents
 import com.cablemc.pokemoncobbled.common.api.reactive.Observable.Companion.filter
 import com.cablemc.pokemoncobbled.common.api.reactive.Observable.Companion.takeFirst
-import com.cablemc.pokemoncobbled.common.client.PokemonCobbledClient
 import com.cablemc.pokemoncobbled.common.net.serverhandling.ServerPacketRegistrar
-import com.cablemc.pokemoncobbled.common.util.ifClient
 import com.cablemc.pokemoncobbled.forge.mod.config.CobbledConfig
 import com.cablemc.pokemoncobbled.forge.mod.net.CobbledForgeNetworkDelegate
 import dev.architectury.platform.forge.EventBuses
-import net.minecraftforge.client.event.ModelBakeEvent
 import net.minecraftforge.common.ForgeMod
-import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.fml.ModLoadingContext
 import net.minecraftforge.fml.config.ModConfig
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent
+import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext
 
 object PokemonCobbledForge : PokemonCobbledModImplementation {
@@ -37,8 +34,7 @@ object PokemonCobbledForge : PokemonCobbledModImplementation {
                 }
 
             addListener(this@PokemonCobbledForge::initialize)
-            addListener(this@PokemonCobbledForge::onBake)
-//            addListener(this@PokemonCobbledForge::onEntityAttributeCreation)
+            addListener(this@PokemonCobbledForge::serverInit)
             CobbledNetwork.networkDelegate = CobbledForgeNetworkDelegate
 
             PokemonCobbled.preinitialize(this@PokemonCobbledForge)
@@ -47,21 +43,18 @@ object PokemonCobbledForge : PokemonCobbledModImplementation {
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, CobbledConfig.spec)
     }
 
+    fun serverInit(event: FMLDedicatedServerSetupEvent) {
+        println("Dedi init")
+        event.enqueueWork {
+            ServerPacketRegistrar.registerHandlers()
+            CobbledNetwork.register()
+        }
+    }
+
     fun initialize(event: FMLCommonSetupEvent) {
         LOGGER.info("Initializing...")
 
         PokemonCobbled.initialize()
 
-        event.enqueueWork {
-            ifClient { PokemonCobbledClient.initialize() }
-            ServerPacketRegistrar.registerHandlers()
-            CobbledNetwork.register()
-        }
-
-        MinecraftForge.EVENT_BUS.register(this)
-    }
-
-    fun onBake(event: ModelBakeEvent) {
-        PokemonCobbledClient.reloadCodedAssets()
     }
 }
