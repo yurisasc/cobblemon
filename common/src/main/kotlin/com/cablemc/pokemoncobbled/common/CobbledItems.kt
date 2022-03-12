47ef1b8b60adcf4dc3b29c453879582dbcc65995
Deltric <d3ltr1c@gmail.com>
2022-03-12 02:11:26 -0600

    Added apricorn seeds + apricorn 2d sprites

--- common/src/main/kotlin/com/cablemc/pokemoncobbled/common/CobbledItems.kt
+++ common/src/main/kotlin/com/cablemc/pokemoncobbled/common/CobbledItems.kt
@@ -9,6 +9,7 @@ import net.minecraft.core.Registry
 import net.minecraft.world.item.BlockItem
 import net.minecraft.world.item.CreativeModeTab
 import net.minecraft.world.item.Item
+import net.minecraft.world.item.ItemNameBlockItem
 import net.minecraft.world.level.block.Block
 
 object CobbledItems {
@@ -27,6 +28,14 @@ object CobbledItems {
     val WHITE_APRICORN = queue("white_apricorn", ApricornItem())
     val YELLOW_APRICORN = queue("yellow_apricorn", ApricornItem())
 
+    val BLACK_APRICORN_SEED = queue("black_apricorn_seed", itemNameBlockItem(CobbledBlocks.BLACK_APRICORN_SAPLING, CreativeModeTab.TAB_MISC))
+    val BLUE_APRICORN_SEED = queue("blue_apricorn_seed", itemNameBlockItem(CobbledBlocks.BLUE_APRICORN_SAPLING, CreativeModeTab.TAB_MISC))
+    val GREEN_APRICORN_SEED = queue("green_apricorn_seed", itemNameBlockItem(CobbledBlocks.GREEN_APRICORN_SAPLING, CreativeModeTab.TAB_MISC))
+    val PINK_APRICORN_SEED = queue("pink_apricorn_seed", itemNameBlockItem(CobbledBlocks.PINK_APRICORN_SAPLING, CreativeModeTab.TAB_MISC))
+    val RED_APRICORN_SEED = queue("red_apricorn_seed", itemNameBlockItem(CobbledBlocks.RED_APRICORN_SAPLING, CreativeModeTab.TAB_MISC))
+    val WHITE_APRICORN_SEED = queue("white_apricorn_seed", itemNameBlockItem(CobbledBlocks.WHITE_APRICORN_SAPLING, CreativeModeTab.TAB_MISC))
+    val YELLOW_APRICORN_SEED = queue("yellow_apricorn_seed", itemNameBlockItem(CobbledBlocks.YELLOW_APRICORN_SAPLING, CreativeModeTab.TAB_MISC))
+
     val APRICORN_LOG = queue("apricorn_log", blockItem(CobbledBlocks.APRICORN_LOG, CreativeModeTab.TAB_BUILDING_BLOCKS))
     val STRIPPED_APRICORN_LOG = queue("stripped_apricorn_log", blockItem(CobbledBlocks.STRIPPED_APRICORN_LOG, CreativeModeTab.TAB_BUILDING_BLOCKS))
     val APRICORN_PLANKS = queue("apricorn_planks", blockItem(CobbledBlocks.APRICORN_PLANKS, CreativeModeTab.TAB_BUILDING_BLOCKS))
@@ -34,6 +43,10 @@ object CobbledItems {
 
     private fun <T : Block> blockItem(supplier: RegistrySupplier<T>, tab: CreativeModeTab) : BlockItem {
         return BlockItem(supplier.get(), Item.Properties().tab(tab))
+    }
+
+    private fun <T : Block> itemNameBlockItem(supplier: RegistrySupplier<T>, tab: CreativeModeTab) : BlockItem {
+        return ItemNameBlockItem(supplier.get(), Item.Properties().tab(tab))
     }
 
     fun register() {
