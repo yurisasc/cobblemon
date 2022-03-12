b290bf92d08aef6ae7ce0c923eaf044983188e3a
Qumoo <me@qumo.at>
2022-03-12 11:47:22 +0100

    Add EvoStone classes

---
+++ common/src/main/kotlin/com/cablemc/pokemoncobbled/common/api/item/EvolutionStoneItem.kt
@@ -0,0 +1,7 @@
+package com.cablemc.pokemoncobbled.common.api.item
+
+import com.cablemc.pokemoncobbled.common.item.CobbledItem
+import net.minecraft.world.item.CreativeModeTab
+
+open class EvolutionStoneItem : CobbledItem(Properties().tab(CreativeModeTab.TAB_MISC)) {
+}
