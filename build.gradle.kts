1c5b0ce0a5932c3d2706861ea3c08f2e3a583fdf
Paul <paul_l_preston@hotmail.com>
2022-03-14 18:57:18 +1000

    Fixed test locations and update gitlab-ci for fabric and forge artifacts

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,12 +59,6 @@ subprojects {
         // meant to be "net.minecraft:minecraft:${rootProject.property("mc_version")}" supposedly, doubtful
         "minecraft"("net.minecraft:minecraft:${rootProject.property("mc_version")}")
         "mappings"(loom.officialMojangMappings())
-
-//        testImplementation("org.junit.jupiter:junit-jupiter-api:5.3.2")
-//        testImplementation("org.junit.jupiter:junit-jupiter-params:5.5.2")
-//        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.3.2")
-//        testImplementation("org.mockito:mockito-core:3.3.3")
-//        testImplementation("io.mockk:mockk:1.12.1")
     }
 
     tasks {
