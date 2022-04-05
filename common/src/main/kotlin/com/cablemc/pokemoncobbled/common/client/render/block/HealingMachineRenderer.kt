d62f4356022be5e1652cf49e79c4e6b9c757e25f
Deltric <d3ltr1c@gmail.com>
2022-04-05 00:11:23 -0500

    ball rendering base

--- common/src/main/kotlin/com/cablemc/pokemoncobbled/common/client/render/block/HealingMachineRenderer.kt
+++ common/src/main/kotlin/com/cablemc/pokemoncobbled/common/client/render/block/HealingMachineRenderer.kt
@@ -2,6 +2,9 @@ package com.cablemc.pokemoncobbled.common.client.render.block
 
 import com.cablemc.pokemoncobbled.common.CobbledBlocks
 import com.cablemc.pokemoncobbled.common.CobbledItems
+import com.cablemc.pokemoncobbled.common.world.level.block.entity.HealingMachineBlockEntity
+import com.mojang.blaze3d.platform.GlStateManager
+import com.mojang.blaze3d.systems.RenderSystem
 import com.mojang.blaze3d.vertex.PoseStack
 import com.mojang.math.Vector3f
 import net.minecraft.client.Minecraft
@@ -11,6 +14,7 @@ import net.minecraft.client.renderer.blockentity.BlockEntityRenderer
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider
 import net.minecraft.core.Direction
 import net.minecraft.world.item.ItemStack
+import net.minecraft.world.item.Items
 import net.minecraft.world.level.block.HorizontalDirectionalBlock
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.state.BlockState
@@ -18,20 +22,43 @@ import net.minecraft.world.level.block.state.BlockState
 class HealingMachineRenderer<T: BlockEntity>(ctx: BlockEntityRendererProvider.Context): BlockEntityRenderer<T> {
     private val healerStack = ItemStack(CobbledItems.HEALING_MACHINE.get())
 
+    companion object {
+        private val offsets = listOf(
+            0.0 to 0.0,
+            -0.64 to 0.0,
+            0.64 to -0.5,
+            -0.64 to 0.0,
+            0.64 to -0.5,
+            -0.64 to 0.0
+        )
+    }
+
     override fun render(blockEntity: T, tickDelta: Float, poseStack: PoseStack, multiBufferSource: MultiBufferSource, light: Int, overlay: Int) {
+        if(blockEntity !is HealingMachineBlockEntity) return
+
         poseStack.pushPose()
 
-        // TODO: Cleanup L
         val blockState =
             if (blockEntity.level != null) (blockEntity as BlockEntity).blockState else (CobbledBlocks.HEALING_MACHINE.get().defaultBlockState().setValue(
                 HorizontalDirectionalBlock.FACING, Direction.SOUTH
             ) as BlockState)
-        val direction = blockState.getValue(HorizontalDirectionalBlock.FACING)
-        val g = direction.toYRot()
+        val yRot = blockState.getValue(HorizontalDirectionalBlock.FACING).toYRot()
 
+        // Position healer
         poseStack.translate(0.5, 0.5, 0.5)
-        poseStack.mulPose(Vector3f.YP.rotationDegrees(-g))
-        Minecraft.getInstance().itemRenderer.renderStatic(healerStack, ItemTransforms.TransformType.GROUND, light, overlay, poseStack, multiBufferSource, 0);
+        poseStack.mulPose(Vector3f.YP.rotationDegrees(-yRot))
+        Minecraft.getInstance().itemRenderer.renderStatic(healerStack, ItemTransforms.TransformType.GROUND, light, overlay, poseStack, multiBufferSource, 0)
+
+        // Position initial ball
+        poseStack.scale(0.5f, 0.5f, 0.5f)
+        poseStack.translate(0.3, 0.4, 0.23)
+
+
+        for((index, pokeBall) in blockEntity.pokeBalls().withIndex()) {
+            val offset = offsets[index]
+            poseStack.translate(offset.first, 0.0, offset.second)
+            Minecraft.getInstance().itemRenderer.renderStatic(ItemStack(CobbledItems.ballMap[pokeBall]?.get() ?: Items.AIR), ItemTransforms.TransformType.GROUND, light, overlay, poseStack, multiBufferSource, 0)
+        }
         poseStack.popPose()
     }
 }
--- common/src/main/kotlin/com/cablemc/pokemoncobbled/common/world/level/block/entity/HealingMachineBlockEntity.kt
+++ common/src/main/kotlin/com/cablemc/pokemoncobbled/common/world/level/block/entity/HealingMachineBlockEntity.kt
@@ -1,7 +1,17 @@
 package com.cablemc.pokemoncobbled.common.world.level.block.entity
 
 import com.cablemc.pokemoncobbled.common.CobbledBlockEntities
+import com.cablemc.pokemoncobbled.common.api.pokeball.PokeBalls
+import com.cablemc.pokemoncobbled.common.pokeball.PokeBall
+import com.cablemc.pokemoncobbled.common.util.getPlayer
+import com.cablemc.pokemoncobbled.common.util.party
+import net.minecraft.ChatFormatting
+import net.minecraft.Util
 import net.minecraft.core.BlockPos
+import net.minecraft.nbt.CompoundTag
+import net.minecraft.network.chat.TextComponent
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket
+import net.minecraft.resources.ResourceLocation
 import net.minecraft.world.level.Level
 import net.minecraft.world.level.block.entity.BlockEntity
 import net.minecraft.world.level.block.entity.BlockEntityTicker
@@ -13,18 +23,117 @@ class HealingMachineBlockEntity(
     blockState: BlockState) : BlockEntity(CobbledBlockEntities.HEALING_MACHINE.get(), blockPos, blockState
 ) {
     private var currentUser: UUID? = null
+    private var pokeBalls: MutableList<PokeBall> = mutableListOf()
+    private var healTimeLeft: Int = 0
+
+    fun currentUser(): UUID? {
+        return this.currentUser
+    }
+
+    fun pokeBalls(): List<PokeBall> {
+        return this.pokeBalls
+    }
 
     fun isInUse(): Boolean {
         return this.currentUser != null
     }
 
     fun setUser(user: UUID) {
+        this.clearData()
+
+        val player = user.getPlayer() ?: return
+        val party = player.party()
+
+        pokeBalls.addAll(party.getAll().map { pokemon -> pokemon.caughtBall })
         this.currentUser = user
+        this.healTimeLeft = 60
+
+        markUpdated()
+    }
+
+    fun completeHealing() {
+        val player = this.currentUser?.getPlayer() ?: return clearData()
+        val party = player.party()
+
+        // TODO: Trigger event
+
+        party.heal()
+        player.sendMessage(TextComponent("${ChatFormatting.GREEN}Your Pokemon have been healed!"), Util.NIL_UUID)
+        this.clearData()
+    }
+
+    private fun clearData() {
+        this.currentUser = null
+        this.pokeBalls.clear()
+        this.healTimeLeft = 0
+        markUpdated()
+    }
+
+    override fun load(compoundTag: CompoundTag) {
+        super.load(compoundTag)
+
+        if(compoundTag.hasUUID("MachineUser")) {
+            this.currentUser = compoundTag.getUUID("MachineUser")
+        }
+        if(compoundTag.contains("MachinePokeBalls")) {
+            val pokeBallsTag = compoundTag.getCompound("MachinePokeBalls")
+            for(key in pokeBallsTag.allKeys) {
+                val pokeBallId = pokeBallsTag.getString(key)
+                if(pokeBallId.isEmpty()) {
+                    continue
+                }
+
+                val pokeBall = PokeBalls.getPokeBall(ResourceLocation(pokeBallId))
+                if(pokeBall != null) {
+                    this.pokeBalls.add(pokeBall)
+                }
+            }
+        }
+        if(compoundTag.contains("MachineTimeLeft")) {
+            this.healTimeLeft = compoundTag.getInt("MachineTimeLeft")
+        }
+    }
+
+    override fun saveAdditional(compoundTag: CompoundTag) {
+        super.saveAdditional(compoundTag)
+
+        compoundTag.putUUID("MachineUser", this.currentUser!!)
+
+        val pokeBallsTag = CompoundTag()
+        var ballIndex = 1
+
+        for(pokeBall in this.pokeBalls) {
+            pokeBallsTag.putString("Pokeball$ballIndex", pokeBall.name.toString())
+            ballIndex++
+        }
+        compoundTag.put("MachinePokeBalls", pokeBallsTag)
+        compoundTag.putInt("MachineTimeLeft", healTimeLeft)
+    }
+
+    override fun getUpdatePacket(): ClientboundBlockEntityDataPacket {
+        return ClientboundBlockEntityDataPacket.create(this)
+    }
+
+    override fun getUpdateTag(): CompoundTag? {
+        return saveWithoutMetadata()
+    }
+
+    private fun markUpdated() {
+        this.setChanged()
+        getLevel()!!.sendBlockUpdated(this.blockPos, blockState, blockState, 3)
     }
 
     companion object : BlockEntityTicker<HealingMachineBlockEntity> {
         override fun tick(level: Level, blockPos: BlockPos, blockState: BlockState, tileEntity: HealingMachineBlockEntity) {
+            if(level.isClientSide) {
+                return
+            }
 
+            if(tileEntity.healTimeLeft > 0) {
+                tileEntity.healTimeLeft--
+                return
+            }
+            tileEntity.completeHealing()
         }
     }
 }
