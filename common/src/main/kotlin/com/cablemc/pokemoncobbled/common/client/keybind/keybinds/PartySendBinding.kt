04a19104a897ec362a78688c19fbf9b92b4a2bdd
BuildTools <unconfigured@null.spigotmc.org>
2022-06-20 20:39:28 -0400

    Added checks to make sure pokemon had HP before sending out

--- common/src/main/kotlin/com/cablemc/pokemoncobbled/common/client/keybind/keybinds/PartySendBinding.kt
+++ common/src/main/kotlin/com/cablemc/pokemoncobbled/common/client/keybind/keybinds/PartySendBinding.kt
@@ -21,7 +21,8 @@ object PartySendBinding : CobbledBlockingKeyBinding(
         val player = MinecraftClient.getInstance().player
         if (PokemonCobbledClient.storage.selectedSlot != -1 && MinecraftClient.getInstance().currentScreen == null && player != null) {
             val pokemon = PokemonCobbledClient.storage.myParty.get(PokemonCobbledClient.storage.selectedSlot)
-            if (pokemon != null) {
+            if (pokemon != null && pokemon.currentHealth > 0 ) {
+            //if (pokemon != null ) {
                 val targetedPokemon = player.traceFirstEntityCollision(entityClass = PokemonEntity::class.java)
                 if (targetedPokemon != null) {
                     if (targetedPokemon.canBattle(player)) {
