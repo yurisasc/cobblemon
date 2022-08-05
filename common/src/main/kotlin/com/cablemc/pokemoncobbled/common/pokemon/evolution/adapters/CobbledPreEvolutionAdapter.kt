57437bf0e5d7221b5318cd7d7c92cea309847333
Licious <goncalo.pereira.santos@hotmail.com>
2022-08-05 15:08:10 +0100

    Fixed compile issue, made PokemonArgumentType support the identifier system.

--- common/src/main/kotlin/com/cablemc/pokemoncobbled/common/pokemon/evolution/adapters/CobbledPreEvolutionAdapter.kt
+++ common/src/main/kotlin/com/cablemc/pokemoncobbled/common/pokemon/evolution/adapters/CobbledPreEvolutionAdapter.kt
@@ -2,6 +2,7 @@ package com.cablemc.pokemoncobbled.common.pokemon.evolution.adapters
 
 import com.cablemc.pokemoncobbled.common.api.pokemon.evolution.PreEvolution
 import com.cablemc.pokemoncobbled.common.pokemon.evolution.CobbledPreEvolution
+import com.cablemc.pokemoncobbled.common.util.asIdentifierDefaultingNamespace
 import com.google.gson.*
 import java.lang.reflect.Type
 
@@ -12,10 +13,10 @@ object CobbledPreEvolutionAdapter : JsonDeserializer<PreEvolution>, JsonSerializ
 
     override fun deserialize(json: JsonElement, typeOfT: Type, context: JsonDeserializationContext): PreEvolution {
         if (json.isJsonPrimitive) {
-            return CobbledPreEvolution(json.asString)
+            return CobbledPreEvolution(json.asString.asIdentifierDefaultingNamespace())
         }
         val jObject = json.asJsonObject
-        return CobbledPreEvolution(jObject.get(SPECIES).asString, jObject.get(FORM).asString)
+        return CobbledPreEvolution(jObject.get(SPECIES).asString.asIdentifierDefaultingNamespace(), jObject.get(FORM).asString)
     }
 
     override fun serialize(src: PreEvolution, typeOfSrc: Type, context: JsonSerializationContext) = JsonObject().apply {
--- common/src/main/kotlin/com/cablemc/pokemoncobbled/common/util/ResourceLocationExtensions.kt
+++ common/src/main/kotlin/com/cablemc/pokemoncobbled/common/util/ResourceLocationExtensions.kt
@@ -12,3 +12,15 @@ fun Identifier.extractTo(directory : File) {
     Files.copy(stream, directory.toPath(), StandardCopyOption.REPLACE_EXISTING)
     stream.close()
 }
+
+/**
+ * Creates an identifier from this string, if a namespace is not present the default [namespace] will be used.
+ *
+ * @param namespace The namespace that will default if none is present, defaults to [PokemonCobbled.MODID].
+ */
+fun String.asIdentifierDefaultingNamespace(namespace: String = PokemonCobbled.MODID): Identifier {
+    val id = this.lowercase()
+    val data = arrayOf(namespace, id)
+    val i = data.indexOf(":")
+    return if (id.contains(":")) Identifier(id.substringBefore(":"), id.substringAfter(":")) else Identifier(namespace, id)
+}
