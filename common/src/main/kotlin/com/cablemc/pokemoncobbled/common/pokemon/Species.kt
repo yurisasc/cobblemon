b47dfece4b07d4ca911dd2ef30b5b07d8f3b33c5
Licious <goncalo.pereira.santos@hotmail.com>
2022-08-08 14:03:53 +0100

    Expose Species.resourceIdentifier

--- common/src/main/kotlin/com/cablemc/pokemoncobbled/common/pokemon/Species.kt
+++ common/src/main/kotlin/com/cablemc/pokemoncobbled/common/pokemon/Species.kt
@@ -55,7 +55,7 @@ class Species {
     internal val preEvolution: PreEvolution? = null
 
     @Transient
-    internal lateinit var resourceIdentifier: Identifier
+    lateinit var resourceIdentifier: Identifier
 
     fun types(form: Int): Iterable<ElementalType> = forms[form].types
 
