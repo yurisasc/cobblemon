5c3eeefa20b793114d0a8d34eebb47824bc10ffc
Paul <Paul_L_Preston@hotmail.com>
2022-08-18 22:51:59 +1000

    Fixed outdated loom version being used

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     base
     kotlin("jvm") version "1.6.0" apply false
     id("architectury-plugin") version "3.4-SNAPSHOT"
-    id("dev.architectury.loom") version "0.11.0-SNAPSHOT" apply false
+    id("dev.architectury.loom") version "0.12.0-SNAPSHOT" apply false
     id("com.github.johnrengelman.shadow") version "7.1.2" apply false
 }
 
