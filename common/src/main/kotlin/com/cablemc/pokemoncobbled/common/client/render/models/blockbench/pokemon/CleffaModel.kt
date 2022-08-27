59d7ccca71dfb09fb2b385b2655c86c792430ae2
EvanMacMillan <moomednick@gmail.com>
2022-08-27 14:53:02 -0400

    Cleffa Line - NOTE: Cleffa's stat json is not included yet.

---
+++ common/src/main/kotlin/com/cablemc/pokemoncobbled/common/client/render/models/blockbench/pokemon/CleffaModel.kt
@@ -0,0 +1,51 @@
+package com.cablemc.pokemoncobbled.common.client.render.models.blockbench.pokemon
+
+import com.cablemc.pokemoncobbled.common.client.render.models.blockbench.PoseableEntityState
+import com.cablemc.pokemoncobbled.common.client.render.models.blockbench.asTransformed
+import com.cablemc.pokemoncobbled.common.client.render.models.blockbench.frame.BiWingedFrame
+import com.cablemc.pokemoncobbled.common.client.render.models.blockbench.frame.HeadedFrame
+import com.cablemc.pokemoncobbled.common.client.render.models.blockbench.pose.TransformedModelPart.Companion.Y_AXIS
+import com.cablemc.pokemoncobbled.common.entity.PoseType
+import com.cablemc.pokemoncobbled.common.entity.pokemon.PokemonEntity
+import net.minecraft.client.model.ModelPart
+import net.minecraft.util.math.Vec3d
+
+class CleffaModel(root: ModelPart) : PokemonPoseableModel() {
+    override val rootPart = root.registerChildWithAllChildren("cleffa")
+
+    override val portraitScale = 1.5F
+    override val portraitTranslation = Vec3d(0.1, -0.02, 0.0)
+
+    override val profileScale = 1.0F
+    override val profileTranslation = Vec3d(0.0, 0.0, 0.0)
+
+
+    lateinit var standing: PokemonPose
+    lateinit var walk: PokemonPose
+
+
+    override fun registerPoses() {
+        standing = registerPose(
+            poseName = "standing",
+            poseTypes = setOf(PoseType.NONE, PoseType.PROFILE, PoseType.PORTRAIT, PoseType.STAND, PoseType.FLOAT),
+            transformTicks = 10,
+            idleAnimations = arrayOf(
+                bedrock("cleffa", "ground_idle")
+            )
+        )
+
+        walk = registerPose(
+            poseName = "walk",
+            poseTypes = setOf(PoseType.WALK, PoseType.SWIM),
+            transformTicks = 10,
+            idleAnimations = arrayOf(
+                bedrock("cleffa", "ground_idle"),
+                bedrock("cleffa", "ground_walk")
+            )
+        )
+    }
+    override fun getFaintAnimation(
+        pokemonEntity: PokemonEntity,
+        state: PoseableEntityState<PokemonEntity>
+    ) = if (state.isPosedIn(standing, walk)) bedrockStateful("cleffa", "faint") else null
+}
