de76affc306be61ff57bd5949e99b35507d8be35
Paul <Paul_L_Preston@hotmail.com>
2022-08-29 14:41:43 +1000

    Rework item evolution so it works with the stones

--- common/src/main/kotlin/com/cablemc/pokemoncobbled/common/item/evo/LeafStone.kt
+++ common/src/main/kotlin/com/cablemc/pokemoncobbled/common/item/evo/LeafStone.kt
@@ -2,6 +2,4 @@ package com.cablemc.pokemoncobbled.common.item.evo
 
 import com.cablemc.pokemoncobbled.common.api.item.EvolutionStoneItem
 
-class LeafStone : EvolutionStoneItem() {
-    // TODO: Evolutions on whatever right click idk...
-}
+class LeafStone : EvolutionStoneItem()
