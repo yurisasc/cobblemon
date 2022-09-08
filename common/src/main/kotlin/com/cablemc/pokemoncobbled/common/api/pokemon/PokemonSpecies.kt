69024ad5dd624e03ca8b011b35221aaefb7c6619
Licious <goncalo.pereira.santos@hotmail.com>
2022-09-08 12:50:14 +0100

    Start of cobbled -> showdown species data injection

--- common/src/main/kotlin/com/cablemc/pokemoncobbled/common/api/pokemon/PokemonSpecies.kt
+++ common/src/main/kotlin/com/cablemc/pokemoncobbled/common/api/pokemon/PokemonSpecies.kt
@@ -19,6 +19,7 @@ import com.cablemc.pokemoncobbled.common.api.pokemon.evolution.requirement.Evolu
 import com.cablemc.pokemoncobbled.common.api.pokemon.experience.ExperienceGroup
 import com.cablemc.pokemoncobbled.common.api.pokemon.experience.ExperienceGroupAdapter
 import com.cablemc.pokemoncobbled.common.api.pokemon.stats.Stat
+import com.cablemc.pokemoncobbled.common.api.pokemon.stats.Stats
 import com.cablemc.pokemoncobbled.common.api.reactive.SimpleObservable
 import com.cablemc.pokemoncobbled.common.api.spawning.condition.TimeRange
 import com.cablemc.pokemoncobbled.common.api.types.ElementalType
@@ -42,6 +43,8 @@ import com.cablemc.pokemoncobbled.common.util.adapters.NbtCompoundAdapter
 import com.cablemc.pokemoncobbled.common.util.adapters.TimeRangeAdapter
 import com.cablemc.pokemoncobbled.common.util.adapters.pokemonPropertiesShortAdapter
 import com.cablemc.pokemoncobbled.common.util.cobbledResource
+import com.caoccao.javet.interop.V8Host
+import com.caoccao.javet.interop.V8Runtime
 import com.google.common.collect.HashBasedTable
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
@@ -181,6 +184,15 @@ object PokemonSpecies : JsonDataRegistry<Species> {
     override fun reload(data: Map<Identifier, Species>) {
         this.speciesByIdentifier.clear()
         this.speciesByDex.clear()
+        val executable = StringBuilder("""
+                const PokemonShowdown = require('pokemon-showdown');
+
+                PokemonShowdown.Dex.modsLoaded = false;
+
+                Object.keys(PokemonShowdown.CobbledPokedex).forEach(key => {
+                    delete PokemonShowdown.CobbledPokedex[key];
+                });
+            """.trimIndent())
         data.forEach { (identifier, species) ->
             species.resourceIdentifier = identifier
             this.speciesByIdentifier.put(identifier, species)?.let { old ->
@@ -188,8 +200,53 @@ object PokemonSpecies : JsonDataRegistry<Species> {
             }
             this.speciesByDex.put(species.resourceIdentifier.namespace, species.nationalPokedexNumber, species)
             species.initialize()
+            this.createShowdownRepresentation(executable, species)
         }
         PokemonCobbled.LOGGER.info("Loaded {} Pokémon species", this.speciesByIdentifier.size)
         this.observable.emit(this)
+        V8Host.getNodeInstance().createV8Runtime<V8Runtime>().use { runtime ->
+            val executor = runtime.getExecutor(executable.toString())
+            executor.resourceName = "./node_modules"
+            executor.executeVoid()
+        }
     }
+
+    private fun createShowdownRepresentation(executable: StringBuilder, species: Species) {
+        // We will use this as the name too as it doesn't really matter for our use case, some properties will also be ignored due to not affecting us
+        val baseSpeciesKey = "${species.resourceIdentifier.namespace}${species.resourceIdentifier.path}".lowercase()
+        // Convert the gender ratio to the appropriate showdown format
+        val genderDetails = when (species.maleRatio) {
+            1F -> "gender: \"F\""
+            0F -> "gender: \"M\""
+            -0.125F -> "gender: \"N\""
+            else -> "genderRatio: { M: ${species.maleRatio}, F: ${1F - species.maleRatio} }"
+        }
+        // ToDo types will need a refresh when we introduce custom typing support
+        // ToDo generate forms data too
+        // ToDo ability conversion
+        // ToDo weight and height on our end as it is necessary for battle mechanics
+        // ToDo Egg groups on our end
+        // ToDo Ability to dynamax on our end
+        // ToDo Signature GMax move on our end
+        executable.append("""
+            PokemonShowdown.CobbledPokedex["$baseSpeciesKey"] = {
+                num: ${species.nationalPokedexNumber},
+                name: "$baseSpeciesKey",
+                types: ["${species.primaryType.name}"${if (species.secondaryType != null) ", \"${species.secondaryType.name}\"" else ""}],
+                $genderDetails,
+                baseStats: { hp: ${species.baseStats[Stats.HP]}, atk: ${species.baseStats[Stats.ATTACK]}, def: ${species.baseStats[Stats.DEFENCE]}, spa: ${species.baseStats[Stats.SPECIAL_ATTACK]}, spd: ${species.baseStats[Stats.SPECIAL_DEFENCE]}, spe: ${species.baseStats[Stats.SPEED]} },
+                abilities: { 0: "Overgrow", H: "Chlorophyll" },
+                heightm: 1,
+                weightkg: 1,
+                color: "White",
+                prevo: "Ivysaur",
+                evos: [],
+                evoLevel: 32,
+                eggGroups: ["Undiscovered"],
+                otherFormes: ["Venusaur-Mega"],
+                formeOrder: ["Venusaur", "Venusaur-Mega"],
+            };
+        """.trimIndent())
+    }
+
 }
