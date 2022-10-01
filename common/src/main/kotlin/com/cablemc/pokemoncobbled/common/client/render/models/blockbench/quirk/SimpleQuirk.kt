61df5fe23eb638649ada5b9f7b42a970b1bf7a50
Paul <Paul_L_Preston@hotmail.com>
2022-10-01 11:34:16 +1000

    License update

--- common/src/main/kotlin/com/cablemc/pokemoncobbled/common/client/render/models/blockbench/quirk/SimpleQuirk.kt
+++ common/src/main/kotlin/com/cablemc/pokemoncobbled/common/client/render/models/blockbench/quirk/SimpleQuirk.kt
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2022 Pokemon Cobbled Contributors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package com.cablemc.pokemoncobbled.common.client.render.models.blockbench.quirk
 
 import com.cablemc.pokemoncobbled.common.client.render.models.blockbench.PoseableEntityState
