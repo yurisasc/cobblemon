a1f09d9982991f794acd4c195924f567d2e5240c
Licious <goncalo.pereira.santos@hotmail.com>
2022-10-02 12:36:23 +0100

    Remove leftover mutation of config in ServerSettingsPacketHandler

--- common/src/main/kotlin/com/cablemc/pokemoncobbled/common/client/net/settings/ServerSettingsPacketHandler.kt
+++ common/src/main/kotlin/com/cablemc/pokemoncobbled/common/client/net/settings/ServerSettingsPacketHandler.kt
@@ -9,7 +9,6 @@
 package com.cablemc.pokemoncobbled.common.client.net.settings
 
 import com.cablemc.pokemoncobbled.common.CobbledNetwork
-import com.cablemc.pokemoncobbled.common.PokemonCobbled
 import com.cablemc.pokemoncobbled.common.client.net.ClientPacketHandler
 import com.cablemc.pokemoncobbled.common.client.settings.ServerSettings
 import com.cablemc.pokemoncobbled.common.net.messages.client.settings.ServerSettingsPacket
@@ -18,9 +17,6 @@ object ServerSettingsPacketHandler : ClientPacketHandler<ServerSettingsPacket> {
 
     override fun invokeOnClient(packet: ServerSettingsPacket, ctx: CobbledNetwork.NetworkContext) {
         ServerSettings.preventCompletePartyDeposit = packet.preventCompletePartyDeposit
-        PokemonCobbled.config.apply {
-            preventCompletePartyDeposit = packet.preventCompletePartyDeposit
-        }
     }
 
 }
