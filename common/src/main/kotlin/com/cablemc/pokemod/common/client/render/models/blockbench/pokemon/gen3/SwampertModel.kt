9d5db12712cb80289e9d152dc5faa0034731b240
Licious <goncalo.pereira.santos@hotmail.com>
2022-10-28 18:18:39 +0100

    Posed Swampert portrait

--- common/src/main/kotlin/com/cablemc/pokemod/common/client/render/models/blockbench/pokemon/gen3/SwampertModel.kt
+++ common/src/main/kotlin/com/cablemc/pokemod/common/client/render/models/blockbench/pokemon/gen3/SwampertModel.kt
@@ -29,8 +29,8 @@ class SwampertModel(root: ModelPart) : PokemonPoseableModel(), HeadedFrame, Bipe
     val armLeft = getPart("arm_left")
     val armRight = getPart("arm_right")
 
-    override val portraitScale = 1.0F
-    override val portraitTranslation = Vec3d(0.0, 0.0, 0.0)
+    override val portraitScale = 1.4F
+    override val portraitTranslation = Vec3d(-.3, 0.45, 0.0)
 
     override val profileScale = 1.0F
     override val profileTranslation = Vec3d(0.0, 0.0, 0.0)
