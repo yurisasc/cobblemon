e02c1688b98f4bca3a2e491845f3332e3bca5c0e
Licious <goncalo.pereira.santos@hotmail.com>
2022-10-28 17:02:54 +0100

    Posed Yanmega portrait

--- common/src/main/kotlin/com/cablemc/pokemod/common/client/render/models/blockbench/pokemon/gen4/YanmegaModel.kt
+++ common/src/main/kotlin/com/cablemc/pokemod/common/client/render/models/blockbench/pokemon/gen4/YanmegaModel.kt
@@ -25,8 +25,8 @@ class YanmegaModel(root: ModelPart) : PokemonPoseableModel(), HeadedFrame {
     override val rootPart = root.registerChildWithAllChildren("yanmega")
     override val head = getPart("head")
 
-    override val portraitScale = 1.0F
-    override val portraitTranslation = Vec3d(0.0, 0.0, 0.0)
+    override val portraitScale = 1.6F
+    override val portraitTranslation = Vec3d(-.5, 0.015, 0.0)
 
     override val profileScale = 1.0F
     override val profileTranslation = Vec3d(0.0, 0.0, 0.0)
