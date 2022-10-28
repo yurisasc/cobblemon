f1bc02b7cfd3dad172defc88de08d1868b45e739
Licious <goncalo.pereira.santos@hotmail.com>
2022-10-28 13:06:11 +0100

    Posed Steenee and Tsareena portraits

--- common/src/main/kotlin/com/cablemc/pokemod/common/client/render/models/blockbench/pokemon/gen7/SteeneeModel.kt
+++ common/src/main/kotlin/com/cablemc/pokemod/common/client/render/models/blockbench/pokemon/gen7/SteeneeModel.kt
@@ -30,8 +30,8 @@ class SteeneeModel(root: ModelPart) : PokemonPoseableModel(), HeadedFrame, Biman
     override val leftLeg = getPart("leg_left")
     override val rightLeg = getPart("leg_right")
 
-    override val portraitScale = 1.0F
-    override val portraitTranslation = Vec3d(0.0, 0.0, 0.0)
+    override val portraitScale = 1.25F
+    override val portraitTranslation = Vec3d(0.15, .5, 0.0)
 
     override val profileScale = 1.0F
     override val profileTranslation = Vec3d(0.0, 0.0, 0.0)
