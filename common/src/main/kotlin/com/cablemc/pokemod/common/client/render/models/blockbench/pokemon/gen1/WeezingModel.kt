5908a8490a8c450c1dfa82fc2aaf705d08a69c03
Licious <goncalo.pereira.santos@hotmail.com>
2022-10-29 12:56:20 +0100

    Posed Weezing portrait

--- common/src/main/kotlin/com/cablemc/pokemod/common/client/render/models/blockbench/pokemon/gen1/WeezingModel.kt
+++ common/src/main/kotlin/com/cablemc/pokemod/common/client/render/models/blockbench/pokemon/gen1/WeezingModel.kt
@@ -18,8 +18,8 @@ import net.minecraft.util.math.Vec3d
 class WeezingModel(root: ModelPart) : PokemonPoseableModel() {
     override val rootPart = root.registerChildWithAllChildren("weezing")
 
-    override val portraitScale = 1.0F
-    override val portraitTranslation = Vec3d(0.0, 0.0, 0.0)
+    override val portraitScale = 1.3F
+    override val portraitTranslation = Vec3d(0.025, 0.15, 0.0)
 
     override val profileScale = 1.0F
     override val profileTranslation = Vec3d(0.0, 0.0, 0.0)
