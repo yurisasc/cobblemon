09734201d0bc6c14bf219fcb388bba468b884fa0
Anne Ominous <unassuminganne@gmail.com>
2022-10-31 13:38:31 -0400

    Fix "totally awful slice" from PlayerSpawner

--- common/src/main/kotlin/com/cablemc/pokemod/common/api/spawning/spawner/PlayerSpawner.kt
+++ common/src/main/kotlin/com/cablemc/pokemod/common/api/spawning/spawner/PlayerSpawner.kt
@@ -13,11 +13,13 @@ import com.cablemc.pokemod.common.api.spawning.SpawnCause
 import com.cablemc.pokemod.common.api.spawning.SpawnerManager
 import com.cablemc.pokemod.common.api.spawning.detail.SpawnPool
 import com.cablemc.pokemod.common.util.getPlayer
+import com.cablemc.pokemod.common.util.squeezeWithinBounds
 import java.util.UUID
 import kotlin.math.atan
 import kotlin.math.cos
 import kotlin.math.sin
 import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.MathHelper.PI
 import net.minecraft.util.math.MathHelper.ceil
 import net.minecraft.util.math.MathHelper.nextBetween
@@ -55,15 +57,27 @@ class PlayerSpawner(player: ServerPlayerEntity, spawns: SpawnPool, manager: Spaw
         val x = center.x + r * cos(theta)
         val z = center.z + r * sin(theta)
 
-        return SpawningArea(
-            cause = cause,
-            world = player.world,
-            baseX = ceil(x - sliceDiameter / 2F),
-            baseY = ceil(center.y - sliceHeight / 2F),
-            baseZ = ceil(z - sliceDiameter / 2F),
-            length = sliceDiameter,
-            height = sliceHeight,
-            width = sliceDiameter
-        )
+        val lowestX = ceil(x - sliceDiameter / 2F)
+        val lowestY = ceil(center.y - sliceHeight / 2F)
+        val lowestZ = ceil(z - sliceDiameter / 2F)
+        val lowestPos = BlockPos(lowestX, lowestY, lowestZ)
+
+        val min = player.world.squeezeWithinBounds(lowestPos)
+        val max = player.world.squeezeWithinBounds(lowestPos.add(sliceDiameter, sliceHeight, sliceDiameter))
+
+        return if (player.world.canSetBlock(min) && player.world.canSetBlock(max)) {
+            SpawningArea(
+                cause = cause,
+                world = player.world,
+                baseX = min.x,
+                baseY = min.y,
+                baseZ = min.z,
+                length = sliceDiameter,
+                height = sliceHeight,
+                width = sliceDiameter
+            )
+        } else {
+            null
+        }
     }
 }
