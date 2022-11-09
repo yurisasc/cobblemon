9f3f3e35b52bd04e7a5eae87913963b912a5bc78
Paul <Paul_L_Preston@hotmail.com>
2022-11-09 18:34:47 +1000

    Script updates to work from new dir [skip-ci]

--- utilityscripts/remove-non-animations.ps1
+++ utilityscripts/remove-non-animations.ps1
@@ -1,3 +1,3 @@
-Get-ChildItem . -Recurse | Where{$_.Name -Match ".bbmodel"} | Remove-Item
-Get-ChildItem . -Recurse | Where{$_.Name -Match ".geo.json"} | Remove-Item
-Get-ChildItem . -Recurse | Where{$_.Name -Match ".png"} | Remove-Item
+Get-ChildItem ./common/src/main/resources/assets/cobblemon/bedrock/animations/ -Recurse | Where{$_.Name -Match ".bbmodel"} | Remove-Item
+Get-ChildItem ./common/src/main/resources/assets/cobblemon/bedrock/animations/ -Recurse | Where{$_.Name -Match ".geo.json"} | Remove-Item
+Get-ChildItem ./common/src/main/resources/assets/cobblemon/bedrock/animations/ -Recurse | Where{$_.Name -Match ".png"} | Remove-Item
--- utilityscripts/remove-non-models.ps1
+++ utilityscripts/remove-non-models.ps1
@@ -1,3 +1,3 @@
-Get-ChildItem . -Recurse | Where{$_.Name -Match ".bbmodel"} | Remove-Item
-Get-ChildItem . -Recurse | Where{$_.Name -Match ".animation.json"} | Remove-Item
-Get-ChildItem . -Recurse | Where{$_.Name -Match ".png"} | Remove-Item
+Get-ChildItem ./common/src/main/resources/assets/cobblemon/bedrock/models/ -Recurse | Where{$_.Name -Match ".bbmodel"} | Remove-Item
+Get-ChildItem ./common/src/main/resources/assets/cobblemon/bedrock/models/ -Recurse | Where{$_.Name -Match ".animation.json"} | Remove-Item
+Get-ChildItem ./common/src/main/resources/assets/cobblemon/bedrock/models/ -Recurse | Where{$_.Name -Match ".png"} | Remove-Item
