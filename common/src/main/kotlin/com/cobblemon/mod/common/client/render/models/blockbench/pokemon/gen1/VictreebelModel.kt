95629b85825ec9ac021c9c631e37572ae5f311ef
Paul <Paul_L_Preston@hotmail.com>
2022-11-12 11:56:04 +1000

    Slightly improve Victrebeel portrait framing

--- common/src/main/kotlin/com/cobblemon/mod/common/client/render/models/blockbench/pokemon/gen1/VictreebelModel.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/client/render/models/blockbench/pokemon/gen1/VictreebelModel.kt
@@ -19,7 +19,7 @@ class VictreebelModel(root: ModelPart) : PokemonPoseableModel() {
     override val rootPart = root.registerChildWithAllChildren("victreebel")
 
     override val portraitScale = 1.9F
-    override val portraitTranslation = Vec3d(0.05, 0.0, 0.0)
+    override val portraitTranslation = Vec3d(0.05, -0.3, 0.0)
 
     override val profileScale = 1.0F
     override val profileTranslation = Vec3d(0.0, 0.0, 0.0)
