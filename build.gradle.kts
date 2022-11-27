45ec1a0983a090b8fc74de45bc4bc0f7c0b43ff6
Nick <nick.degruccio@gmail.com>
2022-11-27 00:55:23 -0800

    Add maven publication support

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,3 +6,8 @@ plugins {
 
 group = "com.cobblemon.mod"
 version = "${project.property("mod_version")}+${project.property("mc_version")}"
+
+val isSnapshot = project.property("snapshot")?.equals("true") ?: false
+if (isSnapshot) {
+    version = "$version-SNAPSHOT"
+}
