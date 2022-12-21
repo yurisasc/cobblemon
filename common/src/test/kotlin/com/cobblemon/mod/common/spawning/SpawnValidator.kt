68478f84d88cf074f1fc8e66b38e96968e6531d7
Nick <nick.degruccio@gmail.com>
2022-12-20 18:01:59 -0800

    Fix Magmortar spawn definitions and add validator for future default configurations

---
+++ common/src/test/kotlin/com/cobblemon/mod/common/spawning/SpawnValidator.kt
@@ -0,0 +1,70 @@
+package com.cobblemon.mod.common.spawning
+
+import com.google.gson.GsonBuilder
+import com.google.gson.JsonObject
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import java.io.FileFilter
+import java.io.FileReader
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.io.path.isRegularFile
+import kotlin.io.path.name
+
+internal class SpawnValidator {
+
+    @Test
+    fun `Validate Spawn Files`() {
+        val root = Paths.get("src")
+            .resolve("main")
+            .resolve("resources")
+            .resolve("data")
+            .resolve("cobblemon")
+
+        val generations = root.resolve("species")
+            .toFile()
+            .listFiles(FileFilter {
+                it.isDirectory
+            })
+
+        val species = mutableListOf<String>()
+        for (generation in generations) {
+            Files.walk(generation.toPath()).filter { it.isRegularFile() }.use {
+                it.forEach { species.add(it.name.removeSuffix(".json")) }
+            }
+        }
+
+        val spawns = root.resolve("spawn_pool_world")
+            .toFile()
+            .listFiles(FileFilter { it.isFile })
+
+        val invalid = mutableListOf<Path>()
+        val gson = GsonBuilder().create()
+        for (spawner in spawns) {
+            val json = gson.fromJson(FileReader(spawner), JsonObject::class.java)
+            val options = json.getAsJsonArray("spawns")
+            for (option in options) {
+                val obj = option as JsonObject
+                var target = obj.get("pokemon").asString
+                if (target.contains(" ")) {
+                    target = target.substring(0, target.indexOf(' '))
+                }
+
+                if (!species.contains(target)) {
+                    invalid.add(spawner.toPath())
+                }
+            }
+        }
+
+        if (invalid.isNotEmpty()) {
+            println("Detected invalid spawn parameters (${invalid.size})")
+            for (marked in invalid) {
+                println("- $marked")
+            }
+        }
+
+        assertTrue(invalid.isEmpty())
+    }
+
+}
