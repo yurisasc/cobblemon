b5d05c4c5599012ac461dca7c6c90fa355443446
Paul <Paul_L_Preston@hotmail.com>
2023-03-17 20:07:28 +1000

    Loads of changelog updates, fix for beam rendering of PokeBalls, @JvmStatic on PokemonItem functions

--- common/src/main/kotlin/com/cobblemon/mod/common/item/PokemonItem.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/item/PokemonItem.kt
@@ -64,12 +64,16 @@ class PokemonItem : CobblemonItem(Settings().maxCount(1)) {
 
     companion object {
 
+        @JvmStatic
         fun from(pokemon: Pokemon, count: Int = 1): ItemStack = from(pokemon.species, pokemon.aspects, count)
 
+        @JvmStatic
         fun from(properties: PokemonProperties, count: Int = 1): ItemStack = from(properties.create(), count)
 
+        @JvmStatic
         fun from(species: Species, vararg aspects: String, count: Int = 1): ItemStack = from(species, aspects.toSet(), count)
 
+        @JvmStatic
         fun from(species: Species, aspects: Set<String>, count: Int = 1): ItemStack {
             val stack = ItemStack(CobblemonItems.POKEMON_MODEL.get(), count)
             stack.orCreateNbt.apply {
