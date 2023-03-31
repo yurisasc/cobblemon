bef8e064f15ac78fbdf6d00506ef73a20ee92b28
Paul <Paul_L_Preston@hotmail.com>
2023-03-31 22:47:18 +1000

    1.3.1 in the Cobblemon object

--- common/src/main/kotlin/com/cobblemon/mod/common/Cobblemon.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/Cobblemon.kt
@@ -128,7 +128,7 @@ import org.apache.logging.log4j.LogManager
 
 object Cobblemon {
     const val MODID = "cobblemon"
-    const val VERSION = "1.3.0"
+    const val VERSION = "1.3.1"
     const val CONFIG_PATH = "config/$MODID/main.json"
     val LOGGER = LogManager.getLogger()
 
