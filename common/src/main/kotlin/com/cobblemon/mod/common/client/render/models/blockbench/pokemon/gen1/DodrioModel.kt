7f90a6ffec9d5f5026f4b3975ceda739a2c14218
Waffle <doctorwafflephd@gmail.com>
2023-06-05 16:00:25 -0500

    Huntail now uses the same "slithering" behavior as Ekans when on land. Doduo and Dodrio now walk significantly faster.

--- common/src/main/kotlin/com/cobblemon/mod/common/client/render/models/blockbench/pokemon/gen1/DodrioModel.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/client/render/models/blockbench/pokemon/gen1/DodrioModel.kt
@@ -53,7 +53,7 @@ class DodrioModel(root: ModelPart) : PokemonPoseableModel(), BipedFrame {
             transformTicks = 10,
             quirks = arrayOf(blink1, blink2, blink3),
             idleAnimations = arrayOf(
-                BipedWalkAnimation(this, periodMultiplier = 0.6F, amplitudeMultiplier = 0.9F),
+                BipedWalkAnimation(this, periodMultiplier = 1.8F, amplitudeMultiplier = 0.9F),
                 bedrock("dodrio", "ground_idle")
 //                singleBoneLook()
                 //bedrock("dodrio", "ground_walk")
--- common/src/main/kotlin/com/cobblemon/mod/common/client/render/models/blockbench/pokemon/gen3/HuntailModel.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/client/render/models/blockbench/pokemon/gen3/HuntailModel.kt
@@ -8,9 +8,13 @@
 
 package com.cobblemon.mod.common.client.render.models.blockbench.pokemon.gen3
 
+import com.cobblemon.mod.common.client.render.models.blockbench.animation.WaveAnimation
+import com.cobblemon.mod.common.client.render.models.blockbench.animation.WaveSegment
 import com.cobblemon.mod.common.client.render.models.blockbench.frame.HeadedFrame
 import com.cobblemon.mod.common.client.render.models.blockbench.pokemon.PokemonPose
 import com.cobblemon.mod.common.client.render.models.blockbench.pokemon.PokemonPoseableModel
+import com.cobblemon.mod.common.client.render.models.blockbench.pose.TransformedModelPart
+import com.cobblemon.mod.common.client.render.models.blockbench.wavefunction.sineFunction
 import com.cobblemon.mod.common.entity.PoseType
 import net.minecraft.client.model.ModelPart
 import net.minecraft.util.math.Vec3d
@@ -29,13 +33,49 @@ class HuntailModel (root: ModelPart) : PokemonPoseableModel(), HeadedFrame {
     lateinit var floating: PokemonPose
     lateinit var swimming: PokemonPose
 
+    private val tail = getPart("tail")
+    private val tail2 = getPart("tail2")
+    private val tail3 = getPart("tail3")
+    private val tail4 = getPart("tail4")
+    private val tail5 = getPart("tail5")
+    private val tail6 = getPart("tail6")
+
+    val tailSegment = WaveSegment(modelPart = tail, length = 5F)
+    val tail2Segment = WaveSegment(modelPart = tail2, length = 5F)
+    val tail3Segment = WaveSegment(modelPart = tail3, length = 5F)
+    val tail4Segment = WaveSegment(modelPart = tail4, length = 5F)
+    val tail5Segment = WaveSegment(modelPart = tail5, length = 5F)
+    val tail6Segment = WaveSegment(modelPart = tail6, length = 5F)
+
     override fun registerPoses() {
         standing = registerPose(
             poseName = "standing",
             poseTypes = PoseType.STANDING_POSES,
             idleAnimations = arrayOf(
                 singleBoneLook(),
-                bedrock("huntail", "ground_idle")
+                bedrock("huntail", "ground_idle"),
+                WaveAnimation(
+                    frame = this,
+                    waveFunction = sineFunction(
+                        period = 8F,
+                        amplitude = 0.8F
+                    ),
+                    basedOnLimbSwing = true,
+                    oscillationsScalar = 5F,
+                    head = head,
+                    rotationAxis = TransformedModelPart.Y_AXIS,
+                    motionAxis = TransformedModelPart.X_AXIS,
+                    moveHead = false,
+                    headLength = 16F,
+                    segments = arrayOf(
+                        tailSegment,
+                        tail2Segment,
+                        tail3Segment,
+                        tail4Segment,
+                        tail5Segment,
+                        tail6Segment
+                    )
+                )
             )
         )
 
