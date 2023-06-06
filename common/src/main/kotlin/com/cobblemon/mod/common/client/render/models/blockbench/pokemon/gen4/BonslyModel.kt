5db7d6380f19cf244ea74a77a09f20fae345d861
Waffle <doctorwafflephd@gmail.com>
2023-06-05 19:38:01 -0500

    Bonsly, Sudowoodoo, Glimmet, & Glimmora implemented.

---
+++ common/src/main/kotlin/com/cobblemon/mod/common/client/render/models/blockbench/pokemon/gen4/BonslyModel.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2023 Cobblemon Contributors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package com.cobblemon.mod.common.client.render.models.blockbench.pokemon.gen4
+
+import com.cobblemon.mod.common.client.render.models.blockbench.animation.BipedWalkAnimation
+import com.cobblemon.mod.common.client.render.models.blockbench.frame.BipedFrame
+import com.cobblemon.mod.common.client.render.models.blockbench.frame.HeadedFrame
+import com.cobblemon.mod.common.client.render.models.blockbench.pokemon.PokemonPose
+import com.cobblemon.mod.common.client.render.models.blockbench.pokemon.PokemonPoseableModel
+import com.cobblemon.mod.common.entity.PoseType.Companion.MOVING_POSES
+import com.cobblemon.mod.common.entity.PoseType.Companion.STATIONARY_POSES
+import com.cobblemon.mod.common.entity.PoseType.Companion.UI_POSES
+import net.minecraft.client.model.ModelPart
+import net.minecraft.util.math.Vec3d
+
+class BonslyModel(root: ModelPart) : PokemonPoseableModel(), HeadedFrame, BipedFrame {
+    override val rootPart = root.registerChildWithAllChildren("bonsly")
+    override val head = getPart("torso")
+
+    override val leftLeg = getPart("leg_left")
+    override val rightLeg = getPart("leg_right")
+
+    override val portraitScale = 2.2F
+    override val portraitTranslation = Vec3d(-0.2, -0.8, 0.0)
+
+    override val profileScale = 0.72F
+    override val profileTranslation = Vec3d(0.0, 0.65, 0.0)
+
+    lateinit var standing: PokemonPose
+    lateinit var walk: PokemonPose
+
+    override fun registerPoses() {
+        val blink = quirk("blink") { bedrockStateful("bonsly", "blink").setPreventsIdle(false) }
+        standing = registerPose(
+            poseName = "standing",
+            poseTypes = STATIONARY_POSES + UI_POSES,
+            quirks = arrayOf(blink),
+            idleAnimations = arrayOf(
+                singleBoneLook(),
+//                bedrock("bonsly", "ground_idle")
+            )
+        )
+
+        walk = registerPose(
+            poseName = "walk",
+            poseTypes = MOVING_POSES,
+            quirks = arrayOf(blink),
+            idleAnimations = arrayOf(
+                singleBoneLook(),
+//                bedrock("bonsly", "ground_idle"),
+                BipedWalkAnimation(this, periodMultiplier = 0.6F, amplitudeMultiplier = 0.9F)
+                //bedrock("bonsly", "ground_walk")
+            )
+        )
+    }
+
+//    override fun getFaintAnimation(
+//        pokemonEntity: PokemonEntity,
+//        state: PoseableEntityState<PokemonEntity>
+//    ) = if (state.isPosedIn(standing, walk)) bedrockStateful("bonsly", "faint") else null
+}
