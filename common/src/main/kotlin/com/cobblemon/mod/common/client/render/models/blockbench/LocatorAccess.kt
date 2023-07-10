2cfa9f898503af0b8169e64908ae0cac82baa34f
Soul Eternum <SoulEternum@gmail.com>
2023-07-10 16:38:51 -0500

    Added a fallback locator for particle animations

--- common/src/main/kotlin/com/cobblemon/mod/common/client/render/models/blockbench/LocatorAccess.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/client/render/models/blockbench/LocatorAccess.kt
@@ -71,6 +71,7 @@ class LocatorAccess(
     fun update(matrixStack: MatrixStack, state: MutableMap<String, MatrixWrapper>) {
         matrixStack.push()
         joint.transform(matrixStack)
+        state.getOrPut("root") { MatrixWrapper() }.updateMatrix(matrixStack.peek().positionMatrix)
 
         for ((name, locator) in locators) {
             matrixStack.push()
