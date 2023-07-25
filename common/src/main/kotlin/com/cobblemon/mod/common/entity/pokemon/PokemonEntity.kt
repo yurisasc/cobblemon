baccb9a9a126f00e471f8929a28fbd1aac07b82b
Paul <Paul_L_Preston@hotmail.com>
2023-07-25 22:18:50 +1000

    Compat 1.20.1

--- common/src/main/kotlin/com/cobblemon/mod/common/entity/pokemon/PokemonEntity.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/entity/pokemon/PokemonEntity.kt
@@ -105,17 +105,12 @@ import net.minecraft.util.ActionResult
 import net.minecraft.util.Hand
 import net.minecraft.util.Identifier
 import net.minecraft.util.math.BlockPos
-import net.minecraft.util.math.Vec2f
 import net.minecraft.util.math.Box
+import net.minecraft.util.math.Vec2f
 import net.minecraft.util.math.Vec3d
 import net.minecraft.world.EntityView
 import net.minecraft.world.World
 import net.minecraft.world.event.GameEvent
-import java.util.*
-import java.util.concurrent.CompletableFuture
-import kotlin.math.round
-import kotlin.math.roundToInt
-import kotlin.math.sqrt
 
 @Suppress("unused")
 class PokemonEntity(
@@ -952,8 +947,8 @@ class PokemonEntity(
         return false
     }
 
-    override fun tickControlled(controllingPassenger: LivingEntity, movementInput: Vec3d) {
-        super.tickControlled(controllingPassenger, movementInput)
+    override fun tickControlled(controllingPlayer: PlayerEntity, movementInput: Vec3d) {
+        super.tickControlled(controllingPlayer, movementInput)
         this.momentum.tick(movementInput)
 
         if (movementInput != Vec3d.ZERO) {
@@ -969,7 +964,7 @@ class PokemonEntity(
             this.seatUpdater.set(this.seats.map { it.toDTO() })
         }
 
-        val vec2f: Vec2f = this.getControlledRotation(controllingPassenger)
+        val vec2f: Vec2f = this.getControlledRotation(controllingPlayer)
         setRotation(vec2f.y, vec2f.x)
         this.headYaw = this.yaw
         this.bodyYaw = this.yaw
@@ -982,13 +977,13 @@ class PokemonEntity(
         return Vec2f(controllingPassenger.pitch * 0.5f, controllingPassenger.yaw)
     }
 
-    override fun updatePassengerPosition(passenger: Entity) {
+    override fun updatePassengerPosition(passenger: Entity, positionUpdater: PositionUpdater) {
         if (this.hasPassenger(passenger)) {
             val seat = this.seats.firstOrNull { it.occupant() == passenger }
             if (seat != null) {
                 val offset = seat.properties.offset.rotateY(-this.bodyYaw * (Math.PI.toFloat() / 180))
-                passenger.setPosition(this.pos.add(offset))
-                if(passenger is LivingEntity) {
+                positionUpdater.accept(passenger, offset.x + passenger.x, offset.y + passenger.y, offset.z + passenger.z)
+                if (passenger is LivingEntity) {
                     passenger.bodyYaw = this.bodyYaw
                 }
             }
@@ -1006,16 +1001,16 @@ class PokemonEntity(
         return null
     }
 
-    override fun updatePassengerForDismount(passenger: LivingEntity?): Vec3d {
+    override fun updatePassengerForDismount(passenger: LivingEntity): Vec3d {
         val seat = this.seats.firstOrNull { it.occupant() == passenger }
         seat?.dismount()
         return super.updatePassengerForDismount(passenger)
     }
 
-    override fun getControlledMovementInput(controllingPassenger: LivingEntity?, movementInput: Vec3d?): Vec3d {
-        super.getControlledMovementInput(controllingPassenger, movementInput)
-        val f = controllingPassenger!!.sidewaysSpeed * 0.2f
-        var g = controllingPassenger.forwardSpeed * 0.5f
+    override fun getControlledMovementInput(controllingPlayer: PlayerEntity, movementInput: Vec3d): Vec3d {
+        super.getControlledMovementInput(controllingPlayer, movementInput)
+        val f = controllingPlayer.sidewaysSpeed * 0.2f
+        var g = controllingPlayer.forwardSpeed * 0.5f
         if (g <= 0.0f) {
             g *= 0.25f
         }
@@ -1023,7 +1018,7 @@ class PokemonEntity(
         return Vec3d(f.toDouble(), 0.0, g.toDouble())
     }
 
-    override fun getSaddledSpeed(controllingPassenger: LivingEntity): Float {
+    override fun getSaddledSpeed(controllingPlayer: PlayerEntity): Float {
 //        val speed = this.capabilities.capability(this)
 //            ?.attribute(RidingAttributeOption.SPEED)
 //            ?.apply()
