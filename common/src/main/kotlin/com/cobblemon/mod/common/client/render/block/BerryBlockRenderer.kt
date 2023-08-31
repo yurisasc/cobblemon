95d6a1c183cf5a2b5d5c03bafe64dd199862fa37
apion <apion@apionservers.com>
2023-08-31 00:08:02 -0400

    More asset sync

--- common/src/main/kotlin/com/cobblemon/mod/common/client/render/block/BerryBlockRenderer.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/client/render/block/BerryBlockRenderer.kt
@@ -42,7 +42,7 @@ class BerryBlockRenderer(private val context: BlockEntityRendererFactory.Context
         for ((berry, growthPoint) in entity.berryAndGrowthPoint()) {
             val model = (if (isFlower) BerryModelRepository.modelOf(berry.flowerModelIdentifier) else BerryModelRepository.modelOf(berry.fruitModelIdentifier)) ?: continue
             val texture = if (isFlower) berry.flowerTexture else berry.fruitTexture
-            val layer = RenderLayer.getEntityCutout(texture)
+            val layer = RenderLayer.getEntityCutoutNoCull(texture)
             model.setAngles(
                 Math.toRadians(180.0 - growthPoint.rotation.x).toFloat(),
                 Math.toRadians(180.0 + growthPoint.rotation.y).toFloat(),
