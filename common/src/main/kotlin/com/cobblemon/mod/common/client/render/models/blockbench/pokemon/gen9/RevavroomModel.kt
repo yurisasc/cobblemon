d120c8e031f4030d9c5d410bcb896e39bdbead69
tyzillion <tylerastry@gmail.com>
2023-09-06 00:22:32 -0400

    Readded Phantump and Trevenants Emissives. License moment 🤙

--- common/src/main/kotlin/com/cobblemon/mod/common/client/render/models/blockbench/pokemon/gen9/RevavroomModel.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/client/render/models/blockbench/pokemon/gen9/RevavroomModel.kt
@@ -1,9 +1,9 @@
 /*
- * Copyright (C) 2123 Cobblemon Contributors
+ * Copyright (C) 2023 Cobblemon Contributors
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.1. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.1/.
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
 package com.cobblemon.mod.common.client.render.models.blockbench.pokemon.gen9
