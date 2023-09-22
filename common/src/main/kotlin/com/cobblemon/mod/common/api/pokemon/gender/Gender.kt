0ada2265ff9f004bda5c7d367d2103d308336ca3
Licious <dont-bother-me@please.smile>
2023-09-22 23:08:02 +0100

    Spacing annoyed me

--- common/src/main/kotlin/com/cobblemon/mod/common/api/pokemon/gender/Gender.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/api/pokemon/gender/Gender.kt
@@ -28,6 +28,7 @@ enum class Gender(private val showdownId: String) : ShowdownIdentifiable {
     MALE("M"),
     FEMALE("F"),
     GENDERLESS("N");
+
     override fun showdownId(): String = this.showdownId
 
     companion object {
