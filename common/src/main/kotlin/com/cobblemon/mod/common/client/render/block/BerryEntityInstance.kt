29c8bbe9e97c122057e83e1ebe16402a530c2ffe
Paul <Paul_L_Preston@hotmail.com>
2023-10-13 18:27:38 +1000

    Will this make the license thing shut up

--- common/src/main/kotlin/com/cobblemon/mod/common/client/render/block/BerryEntityInstance.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/client/render/block/BerryEntityInstance.kt
@@ -5,6 +5,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+class BerryEntityInstance
 //
 //package com.cobblemon.mod.common.client.render.block
 //
