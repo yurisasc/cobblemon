c0ddbad4f58787d43672f05eaf622613a44531e9
Waldleufer <22003632+Waldleufer@users.noreply.github.com>
2023-10-13 22:19:50 +0200

    Pulling fresh data from spreadsheet
    Also added a printout for Troublesome IDs. (missing in the natural language to IDs map)

--- utilityscripts/cobblemon_drops_csv_to_json.py
+++ utilityscripts/cobblemon_drops_csv_to_json.py
@@ -9,17 +9,14 @@ from sqlalchemy import create_engine
 
 def main():
     # Configuration data
-    spreadsheet_csv_url = 'YOUR_SPREADSHEET_PUBLISHED_CSV_URL'
+    spreadsheet_csv_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR51bmzKMTvCfa1UKf454nnlNBCUVMtVNQvxdAiYU09E5pWS7mbsrVt45ABsCGZTByt9N_YEgnSwj8V/pub?gid=0&single=true&output=csv'
     conversion_csv_url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRmvHzUc6_UUKbcvRche7AVebNoljqC1bf3iccusJqW9-C3k0KtESJxOCXShykSejIarAB2jHJ2bHCb/pub?gid=0&single=true&output=csv'
     pokemon_data_dir = '../common/src/main/resources/data/cobblemon/species'
     sqlite_db_name = 'pokemon_drops_data.sqlite'
     sqlite_table_name = 'pokemon_drops'
 
-    # TODO: Download the CSV from the Google Spreadsheet
-    # csv_data = download_spreadsheet_data(spreadsheet_csv_url)
-
-    # For now: just load the data from a local files
-    csv_data = open('Cobblemon Drops - Sheet1.csv', 'r', encoding="utf8").read()
+    # Download the CSV from the Google Spreadsheet
+    csv_data = download_spreadsheet_data(spreadsheet_csv_url)
     csv_data_for_matching = download_spreadsheet_data(conversion_csv_url)
 
     # Load the data into a DataFrame
@@ -63,7 +60,6 @@ def parse_drops(drops_str):
     drops_parts = drops_str.split(', ')
     amount = 0 # Calculate the max amount of drops possible (if everything rolls max)
     noOr = True
-    print(drops_parts)
     if "OR" in drops_str:
         noOr = False
         drops_parts = drops_str.split(" OR ")
@@ -72,6 +68,9 @@ def parse_drops(drops_str):
     for part in drops_parts:
         item_info = part.split(' ')
         item_id = item_info[0]
+
+        if "minecraft:" not in item_id and "cobblemon:" not in item_id:
+            print("Item ID: " + item_id)
 
         currentDrop = {"item": item_id}
 
@@ -92,7 +91,6 @@ def parse_drops(drops_str):
                 pass
             else:
                 quantity = item_info[i]
-                print("Special case: QUANTITY: " + quantity)
                 if quantity != "1":
                     currentDrop.update({"quantityRange": quantity})
                 if noOr:
@@ -140,7 +138,9 @@ def download_spreadsheet_data(url, max_retries=5):
 
 
 def load_data_from_csv(csv_data):
+    print("CSV data:")
     print(csv_data)
+    print("----------- END OF CSV DATA -----------")
     return pd.read_csv(StringIO(csv_data), encoding='utf8', engine='python', dtype={'Pokémon': str, 'Drops': str})
 
 
@@ -156,7 +156,6 @@ def filter_filenames_by_pokemon_names(directory, pokemon_names):
         # While adding, prepend the subdirectory name
         all_files.extend([f"{subdir}/{file}" for file in files_in_subdir])
 
-    print(all_files)
     filtered_files = [file for file in all_files if
                       file.split('/')[-1][:-5].lower() in pokemon_names.str.lower().tolist()]
     return filtered_files
