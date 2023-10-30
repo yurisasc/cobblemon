6a5c52ca0e58b8278faf1af7e9249ab41df9931c
Licious <dont-bother-me@please.smile>
2023-10-30 11:40:47 +0000

    Add the Fairy Feather.

--- common/src/main/kotlin/com/cobblemon/mod/common/item/group/CobblemonItemGroups.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/item/group/CobblemonItemGroups.kt
@@ -317,6 +317,7 @@ object CobblemonItemGroups {
         entries.add(CobblemonItems.DRAGON_FANG)
         entries.add(CobblemonItems.EVERSTONE)
         entries.add(CobblemonItems.EXP_SHARE)
+        entries.add(CobblemonItems.FAIRY_FEATHER)
         entries.add(CobblemonItems.FOCUS_BAND)
         entries.add(CobblemonItems.HARD_STONE)
         entries.add(CobblemonItems.HEAVY_DUTY_BOOTS)
