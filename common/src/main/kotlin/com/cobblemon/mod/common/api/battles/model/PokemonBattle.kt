d4f328157e38f9724034ecb1e30057f63d285c8d
MoeBoy76 <moeboy76@gmail.com>
2023-12-27 23:35:49 +0000

    Update file PokemonBattle.kt

--- common/src/main/kotlin/com/cobblemon/mod/common/api/battles/model/PokemonBattle.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/api/battles/model/PokemonBattle.kt
@@ -63,7 +63,7 @@ open class PokemonBattle(
     val side2: BattleSide
 ) {
     /** Whether logging will be silenced for this battle. */
-    var mute = false
+    var mute = true
 
     init {
         side1.battle = this
