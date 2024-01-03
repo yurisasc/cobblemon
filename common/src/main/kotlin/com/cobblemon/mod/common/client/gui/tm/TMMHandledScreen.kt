b7c1157c5ce164f571b143dcab47c64d02b6bd0d
Lily <enderdusted@gmail.com>
2024-01-03 13:47:41 -0500

    slight changes to the TM UI

--- common/src/main/kotlin/com/cobblemon/mod/common/client/gui/tm/TMMHandledScreen.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/client/gui/tm/TMMHandledScreen.kt
@@ -1,7 +1,6 @@
 package com.cobblemon.mod.common.client.gui.tm
 
 import com.cobblemon.mod.common.api.gui.blitk
-import com.cobblemon.mod.common.client.gui.pc.PCGUI
 import com.cobblemon.mod.common.gui.TMMScreenHandler
 import com.cobblemon.mod.common.util.cobblemonResource
 import net.minecraft.client.gui.DrawContext
@@ -22,11 +21,11 @@ class TMMHandledScreen(
     }
     override fun drawBackground(context: DrawContext, delta: Float, mouseX: Int, mouseY: Int) {
         super.renderBackground(context)
-        val x = (width - 280) / 2
-        val y = (height - 285) / 2
+        val x = (width - TEXTURE_WIDTH) / 2
+        val y = (height - TEXTURE_HEIGHT) / 2
         blitk(
             matrixStack = context.matrices,
-            texture = TEXTURE_IDENTIFIER,
+            texture = TYPE_BASE_TEXTURE,
             x = x+3, y = y,
             width = TEXTURE_WIDTH,
             height = TEXTURE_HEIGHT
@@ -48,6 +47,6 @@ class TMMHandledScreen(
     companion object {
         val TEXTURE_HEIGHT = 285
         val TEXTURE_WIDTH = 280
-        val TEXTURE_IDENTIFIER = cobblemonResource("textures/gui/tm/main.png")
+        val TYPE_BASE_TEXTURE = cobblemonResource("textures/gui/tm/type_selection_base.png")
     }
 }
--- common/src/main/kotlin/com/cobblemon/mod/common/gui/TMMScreenHandler.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/gui/TMMScreenHandler.kt
@@ -1,11 +1,15 @@
 package com.cobblemon.mod.common.gui
 
+import com.cobblemon.mod.common.client.gui.tm.TMMHandledScreen
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.entity.player.PlayerInventory
+import net.minecraft.inventory.CraftingInventory
+import net.minecraft.inventory.Inventory
 import net.minecraft.item.ItemStack
 import net.minecraft.screen.ScreenHandler
 import net.minecraft.screen.ScreenHandlerType
 import net.minecraft.screen.slot.Slot
+import net.minecraft.screen.slot.SlotActionType
 
 class TMMScreenHandler(syncId: Int) : ScreenHandler(CobblemonScreenHandlers.TMM_SCREEN, syncId) {
     var playerEntity: PlayerEntity? = null
@@ -19,22 +23,29 @@ class TMMScreenHandler(syncId: Int) : ScreenHandler(CobblemonScreenHandlers.TMM_
         val xLen = 18
         val yLen = 18
         this.playerInventory = playerInventory
+        val tmInventory = CraftingInventory(this, TMMHandledScreen.TEXTURE_WIDTH, TMMHandledScreen.TEXTURE_HEIGHT)
         for (row in 0..2) {
             for (column in 0..8) {
-                this.addSlot(Slot(playerInventory, 9 + (row * 9) + column, startX + (xLen * column), startY +  (yLen * row)))
+                this.addSlot(Slot(playerInventory, 9 + (row * 9) + column, startX + (xLen * column), startY + (yLen * row)))
             }
         }
         for (column in 0..8) {
             this.addSlot(Slot(playerInventory, column, startX + (xLen * column), startY + 58))
         }
 
+        this.addSlot(Slot(tmInventory, 36, startX + 173, startY - 54))
+
     }
-    override fun quickMove(player: PlayerEntity?, slot: Int): ItemStack {
-        return ItemStack.EMPTY
+    override fun quickMove(player: PlayerEntity, slot: Int): ItemStack {
+        return if (insertItem(getSlot(slot).stack, 0, SLOT_COUNT, true)) getSlot(slot).stack else ItemStack.EMPTY
     }
 
     override fun canUse(player: PlayerEntity?): Boolean {
         return true
+    }
+
+    companion object {
+        val SLOT_COUNT = 37
     }
 
 }
