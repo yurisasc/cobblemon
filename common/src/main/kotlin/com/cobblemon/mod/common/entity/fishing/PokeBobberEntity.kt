929b90173a9ab4925c635dde909b331a3e15263d
jeffw773 <49732785+jeffw773@users.noreply.github.com>
2024-01-12 11:58:02 -0800

    - Some code for fishing testing

---
+++ common/src/main/kotlin/com/cobblemon/mod/common/entity/fishing/PokeBobberEntity.kt
@@ -0,0 +1,15 @@
+/*
+package com.cobblemon.mod.common.entity.fishing
+
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.entity.projectile.FishingBobberEntity
+import net.minecraft.world.World
+
+class PokeBobberEntity : FishingBobberEntity {
+
+    constructor(world: World?, angler: PlayerEntity?, luckOfTheSeaLevel: Int, lureLevel: Int) : super(world, angler, luckOfTheSeaLevel, lureLevel)
+    constructor(world: World?, angler: PlayerEntity?) : super(world, angler)
+    constructor(world: World?) : super(world)
+
+}
+*/
--- common/src/main/kotlin/com/cobblemon/mod/common/item/interactive/PokerodItem.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/item/interactive/PokerodItem.kt
@@ -6,6 +6,8 @@ package net.minecraft.item
 
 import net.minecraft.enchantment.EnchantmentHelper
 import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.item.FishingRodItem
+import net.minecraft.item.Item.Settings
 import net.minecraft.entity.projectile.FishingBobberEntity
 import net.minecraft.sound.SoundCategory
 import net.minecraft.sound.SoundEvents
@@ -15,18 +17,18 @@ import net.minecraft.util.TypedActionResult
 import net.minecraft.world.World
 import net.minecraft.world.event.GameEvent
 
-class PokerodItem(settings: Settings?) : Item(settings), Vanishable {
-    override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
+class PokerodItem(settings: Settings?) : FishingRodItem(settings) {
+    /*override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
         val itemStack = user.getStackInHand(hand)
         val i: Int
-        if (user.fishHook != null) {
+        if (user.fishHook != null) { // if the bobber is out yet
             if (!world.isClient) {
                 i = user.fishHook!!.use(itemStack)
                 itemStack.damage(i, user) { p: PlayerEntity -> p.sendToolBreakStatus(hand) }
             }
             world.playSound(null as PlayerEntity?, user.x, user.y, user.z, SoundEvents.ENTITY_FISHING_BOBBER_RETRIEVE, SoundCategory.NEUTRAL, 1.0f, 0.4f / (world.getRandom().nextFloat() * 0.4f + 0.8f))
             user.emitGameEvent(GameEvent.ITEM_INTERACT_FINISH)
-        } else {
+        } else { // if the bobber is not out yet
             world.playSound(null as PlayerEntity?, user.x, user.y, user.z, SoundEvents.ENTITY_FISHING_BOBBER_THROW, SoundCategory.NEUTRAL, 0.5f, 0.4f / (world.getRandom().nextFloat() * 0.4f + 0.8f))
             if (!world.isClient) {
                 i = EnchantmentHelper.getLure(itemStack)
@@ -36,6 +38,26 @@ class PokerodItem(settings: Settings?) : Item(settings), Vanishable {
             user.incrementStat(Stats.USED.getOrCreateStat(this))
             user.emitGameEvent(GameEvent.ITEM_INTERACT_START)
         }
+        return TypedActionResult.success(itemStack, world.isClient())
+    }*/
+
+    override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
+        val itemStack = user.getStackInHand(hand)
+
+        // Play the throw sound
+        world.playSound(null as PlayerEntity?, user.x, user.y, user.z, SoundEvents.ENTITY_FISHING_BOBBER_THROW, SoundCategory.NEUTRAL, 0.5f, 0.4f / (world.getRandom().nextFloat() * 0.4f + 0.8f))
+
+        // Spawn the FishingBobberEntity directly without checking for an existing one
+        if (!world.isClient) {
+            val i = EnchantmentHelper.getLure(itemStack)
+            val j = EnchantmentHelper.getLuckOfTheSea(itemStack)
+            world.spawnEntity(FishingBobberEntity(user, world, j, i))
+        }
+
+        // Record the usage of the item
+        user.incrementStat(Stats.USED.getOrCreateStat(this))
+        user.emitGameEvent(GameEvent.ITEM_INTERACT_START)
+
         return TypedActionResult.success(itemStack, world.isClient())
     }
 
