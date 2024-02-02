9b06f6a31f3d15ebefc76bc385e8527cc9990679
jeffw773 <49732785+jeffw773@users.noreply.github.com>
2024-02-01 19:52:36 -0800

    - Zooming with scroll wheel works now

--- common/src/main/kotlin/com/cobblemon/mod/common/item/interactive/PhotodexItem.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/item/interactive/PhotodexItem.kt
@@ -8,69 +8,170 @@
 
 package com.cobblemon.mod.common.item.interactive
 
-import com.cobblemon.mod.common.item.CobblemonItem
-import net.minecraft.text.Text
-import net.minecraft.util.hit.EntityHitResult
-import net.minecraft.util.hit.HitResult
 import com.cobblemon.mod.common.entity.pokemon.PokemonEntity
+import net.fabricmc.api.EnvType
+import net.fabricmc.api.Environment
+import net.minecraft.client.MinecraftClient
+import net.minecraft.client.render.*
+import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.entity.Entity
-import net.minecraft.util.math.Box
-import net.minecraft.util.math.Vec3d
-import net.minecraft.client.Mouse
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.ItemStack
 import net.minecraft.item.SpyglassItem
 import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.text.Text
 import net.minecraft.util.Hand
-import net.minecraft.util.TypedActionResult
-import net.minecraft.world.World
-import org.spongepowered.asm.mixin.Mixin
-import org.spongepowered.asm.mixin.injection.At
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable
-import com.mojang.blaze3d.systems.RenderSystem
-import net.minecraft.client.MinecraftClient
-import net.minecraft.client.render.*
 import net.minecraft.util.Identifier
-import net.minecraft.client.util.math.MatrixStack
-import org.lwjgl.opengl.GL11
+import net.minecraft.util.TypedActionResult
+import net.minecraft.util.math.Box
+import net.minecraft.world.World
+import org.lwjgl.glfw.GLFW
+import org.lwjgl.glfw.GLFWMouseButtonCallback
+import org.lwjgl.glfw.GLFWScrollCallback
+import java.util.logging.LogManager
 
 
 class PhotodexItem : /*CobblemonItem(Settings()),*/ SpyglassItem(Settings()) {
+    private var inUse = false
     private var isZooming = false
     private var zoomLevel = 1.0
+    private var scrollCallback = -1L
+    private var mouseCallback = -1L
 
-    override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
-       if (isZooming != true) {
-            if (user is ServerPlayerEntity) {
-                isZooming = true
-                zoomLevel = 1.0
+    private var isScrollCallbackRegistered = false
+    private var isMouseButtonCallbackRegistered = false
 
+    fun changeFOV(double: Double) {
+        val client = MinecraftClient.getInstance()
+        val newFov = (70 / zoomLevel).coerceIn(30.0, 110.0).toInt() // Clamping the FOV value
+        client.options.fov.setValue(newFov)
+    }
 
-                // Send a chat message to the player
-                //user.sendMessage(Text.of("Zooming In"));
+    // todo use method to toggle on functionality of camera
+    /*override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
+        if (world.isClient) {
+            inUse = !inUse // toggle inUse state
 
-                //return super.use(world, user, hand)  // Call the parent class's use method
+            if (inUse) {
+                //registerInputHandlers() // Activate input handling
+                zoomLevel = 1.0 // Initialize zoom level
+            } else {
+                //unregisterInputHandlers() // Deactivate input handling
+                zoomLevel = 1.0 // Reset zoom level
+                changeFOV(70.0) // Reset FOV
             }
-
-           //detectPokemon(world, user, hand)
-
-       }
-        else {
-           isZooming = false
-           zoomLevel = 1.0
-
-           // Send a chat message to the player
-           //user.sendMessage(Text.of("Zooming Out"));
-
+            return TypedActionResult.success(user.getStackInHand(hand))
         }
-        return TypedActionResult.success(user.getStackInHand(hand))
+        return TypedActionResult.pass(user.getStackInHand(hand))
+    }*/
 
+    // todo use method for holding down right click to enable camera like the spyglass
+    override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
+
+        if (world.isClient) {
+            if (!inUse) {
+                inUse = true // Activate only on initial click
+                registerInputHandlers() // Activate input handling
+                zoomLevel = 1.0 // Initialize zoom level
+                changeFOV(70.0) // Set initial FOV
+            }
+            return TypedActionResult.success(user.getStackInHand(hand))
+        }
+        return TypedActionResult.pass(user.getStackInHand(hand))
+    }
+
+    // todo onStoppedUsing for ending the camera use when letting go of the Right Click
+    override fun onStoppedUsing(stack: ItemStack?, world: World, entity: LivingEntity, timeLeft: Int) {
+        super.onStoppedUsing(stack, world, entity, timeLeft)
+        if (world.isClient && entity is PlayerEntity) {
+            inUse = false // Reset on release
+            unregisterInputHandlers()
+            zoomLevel = 1.0
+            changeFOV(70.0) // Reset FOV
+        }
+    }
+
+    @Environment(EnvType.CLIENT)
+    private fun registerInputHandlers() {
+        val windowHandle = MinecraftClient.getInstance().window.handle
+
+        if (!isScrollCallbackRegistered) {
+            // Register scroll callback
+            GLFW.glfwSetScrollCallback(windowHandle) { _, _, yOffset ->
+                println("Scroll Callback Triggered: yOffset = $yOffset")
+
+                if (yOffset != 0.0) {
+                    zoomLevel += yOffset * 0.05 // Smaller increment
+                    zoomLevel = zoomLevel.coerceIn(1.0, 4.0) // More controlled zoom range
+                    changeFOV(70 / zoomLevel)
+                }
+            }
+            isScrollCallbackRegistered = true
+        }
+
+        if (!isMouseButtonCallbackRegistered) {
+            // Register mouse button callback
+            GLFW.glfwSetMouseButtonCallback(windowHandle) { _, button, action, _ ->
+                //println("Mouse Button Callback Triggered")
+                //println("inUse is " + inUse.toString())
+
+                // todo NOTE: BUTTON_1 is apparently LEFT BUTTON_2 is apparently RIGHT but we might need to check
+
+
+                if (inUse && button == GLFW.GLFW_MOUSE_BUTTON_1 && action == GLFW.GLFW_PRESS) {
+                    println("Mouse Button 1 Left Pressed")
+                    MinecraftClient.getInstance().player?.let {
+                        if (it.world.isClient) {
+                            detectPokemon(it.world, it, Hand.MAIN_HAND)
+                        }
+                    }
+                }
+                else if (inUse && button == GLFW.GLFW_MOUSE_BUTTON_1 && action == GLFW.GLFW_RELEASE) {
+                    println("Mouse Button 1 Left Released")
+                    // Implement your logic for release here
+
+                }
+
+                if (inUse && button == GLFW.GLFW_MOUSE_BUTTON_2 && action == GLFW.GLFW_PRESS) {
+                    println("Mouse Button 2 Right Pressed")
+
+                }
+                else if (inUse && button == GLFW.GLFW_MOUSE_BUTTON_2 && action == GLFW.GLFW_RELEASE) {
+                    println("Mouse Button 2 Right Released")
+                    inUse = false
+                    // Implement your logic for release here
+                }
+
+                /*if (inUse && button == GLFW.GLFW_MOUSE_BUTTON_1 && action == GLFW.GLFW_PRESS) {
+                    println("Mouse Button Callback Triggered")
+                    MinecraftClient.getInstance().player?.let {
+                        if (it.world.isClient) {
+                            detectPokemon(it.world, it, Hand.MAIN_HAND)
+                        }
+                    }
+                }*/
+            }
+            isMouseButtonCallbackRegistered = true
+        }
+    }
+
+    private fun unregisterInputHandlers() {
+        val windowHandle = MinecraftClient.getInstance().window.handle
+
+        if (isScrollCallbackRegistered) {
+            GLFW.glfwSetScrollCallback(windowHandle, null)?.free()
+            isScrollCallbackRegistered = false
+        }
+
+        if (isMouseButtonCallbackRegistered) {
+            GLFW.glfwSetMouseButtonCallback(windowHandle, null)?.free()
+            isMouseButtonCallbackRegistered = false
+        }
     }
 
     fun detectPokemon(world: World, user: PlayerEntity, hand: Hand) {
-        if (user is ServerPlayerEntity && isZooming) {
+        if (user is ServerPlayerEntity && inUse) {
             val eyePos = user.getCameraPosVec(1.0F)
             val lookVec = user.getRotationVec(1.0F)
             val maxDistance = 100.0  // Set this to how far you want the ray to go
@@ -109,9 +210,9 @@ class PhotodexItem : /*CobblemonItem(Settings()),*/ SpyglassItem(Settings()) {
         }
     }
 
-    override fun onStoppedUsing(stack: ItemStack, world: World, entity: LivingEntity, timeLeft: Int) {
-        isZooming = false
-    }
+    /*override fun onStoppedUsing(stack: ItemStack, world: World, entity: LivingEntity, timeLeft: Int) {
+        inUse = false
+    }*/
 
     fun renderPhotodexOverlay(matrixStack: MatrixStack) {
         // Assuming your texture is meant to cover the full screen or a significant portion of it.
@@ -129,14 +230,14 @@ class PhotodexItem : /*CobblemonItem(Settings()),*/ SpyglassItem(Settings()) {
         //RenderSystem.enableBlend()
         //RenderSystem.blendFuncSeparate(RenderSystem.SourceFactor.SRC_ALPHA, RenderSystem.DestFactor.ONE_MINUS_SRC_ALPHA, RenderSystem.SourceFactor.ONE, RenderSystem.DestFactor.ZERO)
 
-        RenderSystem.enableBlend()
-        RenderSystem.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA)
+//        RenderSystem.enableBlend()
+//        RenderSystem.blendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA)
 
         // Drawing the overlay
         drawTexture(matrixStack, 0, 0, screenWidth, screenHeight, 0f, 0f, 256, 256, 256, 256)
 
         // Disable blending if you enabled it earlier
-        RenderSystem.disableBlend()
+//        RenderSystem.disableBlend()
     }
 
     // Adjusted to accept float texture coordinates and dimensions
