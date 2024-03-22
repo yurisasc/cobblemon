40761d87dbbfefb45343667b4a4724d5e2273319
Waldleufer <22003632+Waldleufer@users.noreply.github.com>
2024-03-22 19:53:15 +0100

    Adding DexRange as input [skip-ci]

--- utilityscripts/public_spawns_to_json.py
+++ utilityscripts/public_spawns_to_json.py
@@ -55,13 +55,18 @@ def create_filter_toggle_buttons(filter_category, filter_vars, start_row, parent
 
 
 def setup_ui(root):
+    # Variables for storing UI inputs
     input_file_var = tk.StringVar()
     output_dir_var = tk.StringVar()
+    pokemon_number_start_var = tk.IntVar(value=0)  # Default start of dex range
+    pokemon_number_end_var = tk.IntVar(value=1111)  # Default end of dex range
 
     frame = ttk.Frame(root, padding="10")
-    frame.grid(sticky=(tk.W, tk.E, tk.N, tk.S))
+    frame.grid()
+    ttk.Separator(root, orient='horizontal').grid(row=20, column=0, sticky="ew", pady=5)
 
-    filter_vars, process_button = setup_filter_ui(root, input_file_var, output_dir_var)
+    filter_vars, process_button = setup_filter_ui(root, input_file_var, output_dir_var, pokemon_number_start_var,
+                                                  pokemon_number_end_var)
     input_file_label, output_dir_label = setup_file_selection_ui(frame, input_file_var, output_dir_var, process_button)
 
     root.mainloop()
@@ -71,36 +76,41 @@ def setup_file_selection_ui(frame, input_file_var, output_dir_var, process_butto
     input_file_button = ttk.Button(frame, text="Select Input File",
                                    command=lambda: select_input_file(input_file_var, input_file_label, output_dir_var,
                                                                      process_button))
-    input_file_button.grid(column=0, row=0, sticky=(tk.W, tk.E), pady=5)
+    input_file_button.grid(column=0, row=0, pady=5)
 
     input_file_label = ttk.Label(frame, text="No file selected", foreground='red')
-    input_file_label.grid(column=0, row=1, sticky=(tk.W), pady=5)
+    input_file_label.grid(column=0, row=1, pady=5)
 
     output_dir_button = ttk.Button(frame, text="Select Output Directory",
                                    command=lambda: select_output_directory(output_dir_var, output_dir_label,
                                                                            input_file_var, process_button))
-    output_dir_button.grid(column=0, row=2, sticky=(tk.W, tk.E), pady=5)
+    output_dir_button.grid(column=0, row=2, pady=5)
 
     output_dir_label = ttk.Label(frame, text="No directory selected", foreground='red')
-    output_dir_label.grid(column=0, row=3, sticky=(tk.W), pady=5)
+    output_dir_label.grid(column=0, row=3, pady=5)
 
     return input_file_label, output_dir_label
 
 
-def setup_filter_ui(root, input_file_var, output_dir_var):
-    frame2 = ttk.Frame(root, padding="10")
-    frame2.grid(sticky=(tk.W, tk.E, tk.N, tk.S))
+def setup_filter_ui(root, input_file_var, output_dir_var, pokemon_number_start_var, pokemon_number_end_var):
 
-    # Define your filter categories with names and default values
+    frame3 = ttk.Frame(root, padding="10")
+    frame3.grid(sticky=(tk.W, tk.E, tk.N, tk.S))
+
+    # Label and entry for specifying the range of Pokémon numbers to include
+    ttk.Label(frame3, text="Pokémon Number Range:").grid(column=0, row=0, columnspan=5, pady=5)
+    ttk.Entry(frame3, textvariable=pokemon_number_start_var, width=5).grid(column=1, row=1, pady=5)
+    ttk.Label(frame3, text="-").grid(column=2, row=1, pady=5)
+    ttk.Entry(frame3, textvariable=pokemon_number_end_var, width=5).grid(column=3, row=1, pady=5)
+
     filter_vars = {
         'included_groups': {name: tk.IntVar(value=1) for name in ['basic', 'boss', 'fossil']},
         'known_contexts': {name: tk.IntVar(value=1) for name in ['grounded', 'submerged', 'seafloor', 'surface']},
         'bucket_mapping': {name: tk.IntVar(value=1) for name in ['common', 'uncommon', 'rare', 'ultra-rare']},
         'included_generations': {str(gen): tk.IntVar(value=1) for gen in range(1, 10)}
-        # Assuming generations 1 through 9
     }
 
-    # Create custom styles for toggle buttons
+    # Custom styles for toggle buttons
     style = ttk.Style()
     style.configure('Deactivated.TButton', background='gray', foreground='gray', font=('Helvetica', 10, 'overstrike'))
     style.configure('Active.TButton', background='#4CAF50', foreground='black', font=('Helvetica', 10))
@@ -113,28 +123,24 @@ def setup_filter_ui(root, input_file_var, output_dir_var):
         ('included_generations', "Generations")
     ]
 
-    current_row = 0
-    # Add a descriptive text before the filter buttons
-    ttk.Label(frame2, text="Toggle the following filters to exclude certain categories of cobblemon:").grid(column=0,
-                                                                                                            row=current_row,
-                                                                                                            columnspan=5,
-                                                                                                            sticky=(
-                                                                                                            tk.W, tk.E),
-                                                                                                            pady=5)
+    current_row = 2
+    ttk.Label(frame3, text="Toggle the following filters to exclude certain "
+                           "categories of cobblemon:").grid(column=0, row=current_row, columnspan=5, pady=5)
+    current_row += 1
     for category, label_text in filter_sections:
-        ttk.Label(frame2, text=f"{label_text}:").grid(column=0, row=current_row + 1, sticky=tk.W, pady=2)
+        ttk.Label(frame3, text=f"{label_text}:").grid(column=0, row=current_row + 1, sticky=tk.W, pady=2)
         current_row += 1
-        current_row = create_filter_toggle_buttons(category, filter_vars, current_row + 2, parent_frame=frame2)
+        current_row = create_filter_toggle_buttons(category, filter_vars, current_row + 2, parent_frame=frame3)
 
-    # Create process_button before calling setup_filter_ui
-    process_button = ttk.Button(frame2, text="Execute Spawns Writer",
-                                command=lambda: process_file(input_file_var.get(), output_dir_var.get(),
-                                                             filter_vars),
+    ttk.Separator(frame3, orient='horizontal').grid(row=current_row + 1, column=0, columnspan=5, sticky="ew", pady=5)
+    current_row += 1
+
+    process_button = ttk.Button(frame3, text="Execute Spawns Writer",
+                                command=lambda: process_file(input_file_var.get(), output_dir_var.get(), filter_vars),
                                 state='disabled')
-    process_button.grid(column=0, row=current_row + 1, columnspan=5, sticky=(tk.W, tk.E),
-                        pady=5)  # Re-add it below the filters
+    process_button.grid(column=0, row=current_row + 1, columnspan=5, sticky=(tk.W, tk.E), pady=5)
 
-    return filter_vars, process_button  # Return filter_vars for potential future use
+    return filter_vars, process_button
 
 
 if __name__ == "__main__":
