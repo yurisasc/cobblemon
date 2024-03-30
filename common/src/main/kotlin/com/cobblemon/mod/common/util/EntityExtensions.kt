9f6c44fd75d2891a69e09e67d0d68b84339cba95
Flarkinater <flarkinater@gmail.com>
2024-03-29 22:08:40 -0400

    Sendout location is only shifted vertically when necessary

--- common/src/main/kotlin/com/cobblemon/mod/common/util/EntityExtensions.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/util/EntityExtensions.kt
@@ -13,24 +13,42 @@ import net.minecraft.entity.data.DataTracker
 import net.minecraft.entity.data.TrackedData
 import net.minecraft.util.function.BooleanBiFunction
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Box
 import net.minecraft.util.math.Direction
 import net.minecraft.util.math.Vec3d
 import net.minecraft.util.shape.VoxelShapes
 
 fun Entity.setPositionSafely(pos: Vec3d): Boolean {
+    // TODO: Rework this function. Best way to do it would be to define a vertical and horizontal min/max shift based on the Pokemon's hitbox.
+    // Then loop through horizontal/vertical shifts, and detect collisions in three categories: suffocation, damaging blocks, and general collision
+    // The closest position with the least severe collision types will be selected to move the Pokemon to
+    // The throw could be cancelled if there are no viable locations without severe problems
+
+    // Optional: use getBlockCollisions iterator and VoxelShapes.combineAndSimplify to create a single cube to represent collision area
+    // Use that cube to "push" the Pokemon out of the wall at an angle
+    // Note: may not work well with L shape wall collisions
+//    val collisions = world.getBlockCollisions(this, box).iterator()
+//    if (collisions.hasNext()) {
+//        var collisionShape = collisions.next()
+//        while (collisions.hasNext()) {
+//            collisionShape = VoxelShapes.union(collisionShape, collisions.next())
+//            println(collisionShape)
+//        }
+//    } else {
+//        setPosition(pos)
+//        return true
+//    }
+
     var result = pos
     val eyes = pos.withAxis(Direction.Axis.Y, pos.y + this.standingEyeHeight)
 
     var box = boundingBox.offset(pos)
     val conflicts = mutableSetOf<Direction>()
 
-    if (!world.getCollisions(this, box).iterator().hasNext()) {
+    if (!world.getBlockCollisions(this, box).iterator().hasNext()) {
         setPosition(pos)
         return true
     }
-
-    // TODO: use getCollisions iterator and VoxelShapes.combineAndSimplify to create a single cube to represent collision area
-    // Use that cube to "push" the Pokemon out of the wall at an angle
 
     for (target in BlockPos.stream(box)) {
         val blockState = this.world.getBlockState(target)
@@ -72,9 +90,14 @@ fun Entity.setPositionSafely(pos: Vec3d): Boolean {
     }
 
     box = boundingBox.offset(result)
-    if (world.getCollisions(this, box).iterator().hasNext()) {
+    if (!world.getBlockCollisions(this, box).iterator().hasNext()) {
+        this.setPosition(result)
+        return true
+    } else {
         val yChanges = listOf(1.0, -1.0, 2.0, -2.0)
         var previousChange = 0.0
+
+        // If the Pokemon still collides with blocks after horizontal shifting, try vertical shifting from the shifted position
         for (yChange in yChanges) {
             box = box.offset(0.0, yChange - previousChange, 0.0)
             previousChange = yChange
@@ -86,8 +109,36 @@ fun Entity.setPositionSafely(pos: Vec3d): Boolean {
                 // If the rounded position actually collides again, then don't round at all.
                 if (world.getBlockCollisions(this, box).iterator().hasNext()) {
                     result = result.add(0.0, yChange, 0.0)
+                    this.setPosition(result)
+                    return true
                 } else {
                     result = Vec3d(result.x, roundedY.toDouble(), result.z)
+                    this.setPosition(result)
+                    return true
+                }
+            }
+        }
+
+        // If vertical shifting from the new position still collides, try vertical shifting from the original pos
+        previousChange = 0.0
+        box = boundingBox.offset(pos)
+        for (yChange in yChanges) {
+            box = box.offset(0.0, yChange - previousChange, 0.0)
+            previousChange = yChange
+            if (world.getBlockCollisions(this, box).iterator().hasNext()) {
+                continue
+            } else {
+                val roundedY = (result.y + yChange).toInt()
+                box = box.offset(0.0, roundedY - result.y, 0.0)
+                // If the rounded position actually collides again, then don't round at all.
+                if (world.getBlockCollisions(this, box).iterator().hasNext()) {
+                    result = result.add(0.0, yChange, 0.0)
+                    this.setPosition(result)
+                    return true
+                } else {
+                    result = Vec3d(result.x, roundedY.toDouble(), result.z)
+                    this.setPosition(result)
+                    return true
                 }
             }
         }
@@ -97,8 +148,31 @@ fun Entity.setPositionSafely(pos: Vec3d): Boolean {
         this.setPosition(pos)
     }
 
-    this.setPosition(result)
-    return true
+    // This final check guarantees that the sendout will return to the original position if the Pokemon will suffocate in the new one
+    // This will only happen if the horizontal shift moved the Pokemon into a suffocating position, and there was no valid vertical shift
+    val resultEyes = result.withAxis(Direction.Axis.Y, result.y + this.standingEyeHeight)
+    val resultEyeBox = Box.of(resultEyes, width.toDouble(), 1.0E-6, width.toDouble())
+    var collides = false
+
+    for (target in BlockPos.stream(resultEyeBox)) {
+        val blockState = this.world.getBlockState(target)
+        collides = !blockState.isAir &&
+                blockState.shouldSuffocate(this.world, target) &&
+                VoxelShapes.matchesAnywhere(
+                    blockState.getCollisionShape(this.world, target)
+                        .offset(target.x.toDouble(), target.y.toDouble(), target.z.toDouble()),
+                    VoxelShapes.cuboid(box),
+                    BooleanBiFunction.AND
+                )
+        if (collides) break
+    }
+    if (collides) {
+        this.setPosition(pos)
+        return true
+    } else {
+        this.setPosition(result)
+        return true
+    }
 }
 
 fun Entity.distanceTo(pos: BlockPos): Double {
