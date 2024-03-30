9b49f5e01f13749bb8c44c56f4fb1e97ce51f3ad
Segfault Guy <mrfakest@gmail.com>
2024-03-30 15:37:37 -0400

    Add licenses.

--- common/src/main/kotlin/com/cobblemon/mod/common/battles/interpreter/instructions/DragInstruction.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/battles/interpreter/instructions/DragInstruction.kt
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2024 Cobblemon Contributors
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package com.cobblemon.mod.common.battles.interpreter.instructions
 
 import com.cobblemon.mod.common.api.battles.interpreter.BattleContext
