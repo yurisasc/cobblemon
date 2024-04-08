8aeeece0a3a055bef476985c0233e70ffabaa58f
Sterrezo <sterrezo@protonmail.com>
2024-04-08 18:55:58 +0200

    Fixed missing cries, updated the changelog.
    Fixed missing audio files, sound effect keyframes and model.kt files. Partially updated the changelog.

--- common/src/main/kotlin/com/cobblemon/mod/common/client/render/models/blockbench/pokemon/gen3/LileepModel.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/client/render/models/blockbench/pokemon/gen3/LileepModel.kt
@@ -9,6 +9,7 @@
 package com.cobblemon.mod.common.client.render.models.blockbench.pokemon.gen3
 
 import com.cobblemon.mod.common.client.render.models.blockbench.frame.HeadedFrame
+import com.cobblemon.mod.common.client.render.models.blockbench.pokemon.CryProvider
 import com.cobblemon.mod.common.client.render.models.blockbench.pokemon.PokemonPose
 import com.cobblemon.mod.common.client.render.models.blockbench.pokemon.PokemonPoseableModel
 import com.cobblemon.mod.common.entity.PoseType
@@ -30,6 +31,8 @@ class LileepModel (root: ModelPart) : PokemonPoseableModel(), HeadedFrame {
     lateinit var sleep: PokemonPose
     lateinit var waterIdle: PokemonPose
     lateinit var waterSwim: PokemonPose
+
+    override val cryAnimation = CryProvider { _, _ -> bedrockStateful("lileep", "cry") }
 
     override fun registerPoses() {
         val blink = quirk { bedrockStateful("lileep", "blink") }
