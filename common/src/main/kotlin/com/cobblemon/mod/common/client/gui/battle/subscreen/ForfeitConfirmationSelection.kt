56f86e15aef2a5fce36da4760b68365247df146a
JazzMcNade <pharoalexander@gmail.com>
2024-04-21 13:59:50 -0500

    Selecting forfeit in a double/triple battle now short circuits any other pending action requests

--- common/src/main/kotlin/com/cobblemon/mod/common/client/gui/battle/subscreen/ForfeitConfirmationSelection.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/client/gui/battle/subscreen/ForfeitConfirmationSelection.kt
@@ -9,6 +9,9 @@
 package com.cobblemon.mod.common.client.gui.battle.subscreen
 
 import com.cobblemon.mod.common.battles.ForfeitActionResponse
+import com.cobblemon.mod.common.battles.PassActionResponse
+import com.cobblemon.mod.common.client.CobblemonClient
+import com.cobblemon.mod.common.client.battle.ClientBattle
 import com.cobblemon.mod.common.client.battle.SingleActionRequest
 import com.cobblemon.mod.common.client.gui.battle.BattleGUI
 import com.cobblemon.mod.common.client.gui.battle.BattleOverlay
@@ -40,6 +43,13 @@ class ForfeitConfirmationSelection(
 
         forfeitButton = BattleOptionTile(battleGUI, x, y, BattleGUI.runResource, battleLang("ui.forfeit")) {
             battleGUI.selectAction(request, ForfeitActionResponse())
+
+            // Need to fill out any other pending requests
+            var pendingRequest = CobblemonClient.battle?.getFirstUnansweredRequest()
+            while (pendingRequest != null) {
+                battleGUI.selectAction(pendingRequest, PassActionResponse)
+                pendingRequest = CobblemonClient.battle?.getFirstUnansweredRequest()
+            }
             playDownSound(MinecraftClient.getInstance().soundManager)
         }
     }
