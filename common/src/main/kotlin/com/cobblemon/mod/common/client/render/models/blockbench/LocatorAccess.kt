31136435fe2b805d7e2aafeba8f06932e285f4bd
Paul <Paul_L_Preston@hotmail.com>
2024-04-25 10:20:19 +1000

    Fixed autogeneration of target and special_attack locators

--- common/src/main/kotlin/com/cobblemon/mod/common/client/render/models/blockbench/LocatorAccess.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/client/render/models/blockbench/LocatorAccess.kt
@@ -82,7 +82,7 @@ class LocatorAccess(
             // Put in an approximation of the target locator. If the model has one defined,
             // this will be overridden.
             matrixStack.push()
-            matrixStack.translate(0.0, -entity.boundingBox.yLength / 2.0 / scale + 1.5F, -entity.width * 0.6 / scale)
+            matrixStack.translate(0.0, -entity.boundingBox.yLength / 2.0 / scale, -entity.width * 0.6 / scale)
             matrixStack.scale(-1F, -1F, 1F)
             state.getOrPut("target") { MatrixWrapper() }.updateMatrix(matrixStack.peek().positionMatrix)
             state.getOrPut("special_attack") { MatrixWrapper() }.updateMatrix(matrixStack.peek().positionMatrix)
