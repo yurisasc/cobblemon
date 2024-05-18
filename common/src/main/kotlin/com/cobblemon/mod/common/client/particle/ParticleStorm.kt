26a72fe6a93da8cbe308f17c61520dc9e070163b
apion <apion@apionservers.com>
2024-05-18 18:46:58 -0400

    I hope this works (pt. 2)

--- common/src/main/kotlin/com/cobblemon/mod/common/client/particle/ParticleStorm.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/client/particle/ParticleStorm.kt
@@ -53,9 +53,13 @@ class ParticleStorm(
                 .addFunction("entity_size") { DoubleValue(entity.boundingBox.run { if (xLength > yLength) xLength else yLength }) }
                 .addFunction("entity_radius") { DoubleValue(entity.boundingBox.run { if (xLength > yLength) xLength else yLength } / 2) }
                 .addFunction("entity_scale") {
-                    val pokemon = (entity as? PokemonEntity)?.pokemon
-                    val scale = pokemon?.form?.baseScale ?: pokemon?.species?.baseScale ?: 1.0
-                    DoubleValue(scale)
+                    val pokeEntity = entity as? PokemonEntity
+                    val pokemon = pokeEntity?.pokemon
+                    //Use form data if available, species as fall back
+                    val baseScale = pokemon?.form?.baseScale ?: pokemon?.species?.baseScale ?: 1.0F
+                    val pokemonScale = pokemon?.scaleModifier ?: 1.0F
+                    val entityScale = pokeEntity?.scaleFactor ?: 1.0F
+                    DoubleValue(baseScale * pokemonScale * entityScale)
                 }
             // TODO replace with a generified call to if (entity is MoLangEntity) entity.applyVariables(env) or w/e
             runtime.environment.setSimpleVariable("entity_width", DoubleValue(entity.boundingBox.xLength))
