ec28ca0c45ea0bbad6291338ba4ef22d138042c3
apion <apion@apionservers.com>
2024-05-19 01:10:17 -0400

    Added action effect for paralysis + action effect triggers for the cant command

--- common/src/main/kotlin/com/cobblemon/mod/common/battles/interpreter/instructions/CantInstruction.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/battles/interpreter/instructions/CantInstruction.kt
@@ -8,14 +8,23 @@
 
 package com.cobblemon.mod.common.battles.interpreter.instructions
 
+import com.bedrockk.molang.runtime.MoLangRuntime
 import com.cobblemon.mod.common.api.battles.interpreter.BattleMessage
 import com.cobblemon.mod.common.api.battles.model.PokemonBattle
+import com.cobblemon.mod.common.api.molang.MoLangFunctions.addStandardFunctions
+import com.cobblemon.mod.common.api.molang.MoLangFunctions.getQueryStruct
+import com.cobblemon.mod.common.api.moves.animations.ActionEffectContext
+import com.cobblemon.mod.common.api.moves.animations.TargetsProvider
+import com.cobblemon.mod.common.api.moves.animations.UsersProvider
 import com.cobblemon.mod.common.api.pokemon.status.Statuses
 import com.cobblemon.mod.common.api.text.red
 import com.cobblemon.mod.common.api.text.text
+import com.cobblemon.mod.common.battles.dispatch.GO
 import com.cobblemon.mod.common.battles.dispatch.InterpreterInstruction
+import com.cobblemon.mod.common.battles.dispatch.UntilDispatch
 import com.cobblemon.mod.common.util.battleLang
 import com.cobblemon.mod.common.util.lang
+import java.util.concurrent.CompletableFuture
 
 /**
  * Formats: |cant|POKEMON|REASON and |cant|POKEMON|REASON|MOVE
@@ -25,6 +34,8 @@ import com.cobblemon.mod.common.util.lang
  * @since January 22nd, 2022
  */
 class CantInstruction(val message: BattleMessage): InterpreterInstruction {
+    var future = CompletableFuture.completedFuture(Unit)
+    var holds = mutableSetOf<String>()
 
     override fun invoke(battle: PokemonBattle) {
         battle.dispatchWaiting {
@@ -46,6 +57,27 @@ class CantInstruction(val message: BattleMessage): InterpreterInstruction {
 
             battle.broadcastChatMessage(lang.red())
             battle.minorBattleActions[pokemon.uuid] = message
+
+            //Is this nested dispatch ok?
+            battle.dispatch {
+                val status = Statuses.getStatus(effectID)
+                val actionEffect = status?.getActionEffect() ?: return@dispatch GO
+                val providers = mutableListOf<Any>(battle)
+                pokemon.effectedPokemon.entity?.let { UsersProvider(it) }?.let(providers::add)
+                val runtime = MoLangRuntime().also {
+                    battle.addQueryFunctions(it.environment.getQueryStruct()).addStandardFunctions()
+                }
+
+                val context = ActionEffectContext(
+                    actionEffect = actionEffect,
+                    runtime = runtime,
+                    providers = providers
+                )
+                this.future = actionEffect.run(context)
+                holds = context.holds // Reference so future things can check on this action effect's holds
+                future.thenApply { holds.clear() }
+                return@dispatch UntilDispatch { "effects" !in context.holds }
+            }
         }
     }
 }
