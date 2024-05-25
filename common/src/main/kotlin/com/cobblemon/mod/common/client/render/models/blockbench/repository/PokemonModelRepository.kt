bb9fcc64ed129412b819527a1d12d5d47dc5d7a9
apion <apion@apionservers.com>
2024-05-24 21:52:16 -0400

    Started working on Aerodactyl JSON poser

--- common/src/main/kotlin/com/cobblemon/mod/common/client/render/models/blockbench/repository/PokemonModelRepository.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/client/render/models/blockbench/repository/PokemonModelRepository.kt
@@ -82,7 +82,6 @@ object PokemonModelRepository : VaryingModelRepository<PokemonEntity, PokemonPos
         inbuilt("machop", ::MachopModel)
         inbuilt("machoke", ::MachokeModel)
         inbuilt("machamp", ::MachampModel)
-        inbuilt("aerodactyl", ::AerodactylModel)
         inbuilt("alakazam", ::AlakazamModel)
         inbuilt("arbok", ::ArbokModel)
         inbuilt("arcanine", ::ArcanineModel)
