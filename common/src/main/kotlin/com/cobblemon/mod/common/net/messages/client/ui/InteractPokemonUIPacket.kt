6fb38fc70c1776870f9fb103366f327556b93fa4
Paul <Paul_L_Preston@hotmail.com>
2024-06-14 15:12:48 +1000

    Correction

--- common/src/main/kotlin/com/cobblemon/mod/common/net/messages/client/ui/InteractPokemonUIPacket.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/net/messages/client/ui/InteractPokemonUIPacket.kt
@@ -24,17 +24,18 @@ import java.util.UUID
  * @author Village
  * @since January 7th, 2023
  */
-class InteractPokemonUIPacket(val pokemonID: UUID, val canMountShoulder: Boolean): NetworkPacket<InteractPokemonUIPacket> {
+class InteractPokemonUIPacket(val pokemonID: UUID, val canMountShoulder: Boolean, val canRide: Boolean): NetworkPacket<InteractPokemonUIPacket> {
 
     override val id = ID
 
     override fun encode(buffer: RegistryByteBuf) {
         buffer.writeUuid(pokemonID)
         buffer.writeBoolean(canMountShoulder)
+        buffer.writeBoolean(canRide)
     }
 
     companion object {
         val ID = cobblemonResource("interact_pokemon_ui")
-        fun decode(buffer: RegistryByteBuf) = InteractPokemonUIPacket(buffer.readUuid(), buffer.readBoolean())
+        fun decode(buffer: RegistryByteBuf) = InteractPokemonUIPacket(buffer.readUuid(), buffer.readBoolean(), buffer.readBoolean())
     }
 }
