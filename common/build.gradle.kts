d6872df83100cf5aa0af4145b8f31a472c81acbc
Polymeta <4726322-Polymeta@users.noreply.gitlab.com>
2024-07-21 07:47:52 +0000

    bump versioning gradle plugin

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -20,7 +20,7 @@ plugins {
 
     id("net.kyori.blossom")
     id("org.jetbrains.gradle.plugin.idea-ext")
-    id("net.nemerosa.versioning") version "2.8.2"
+    id("net.nemerosa.versioning") version "3.1.0"
 }
 
 architectury {
